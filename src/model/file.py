cb23c817963314325a6ddad5eddf870ced8c4a97
estani@miklip <estanislao.gonzalez@met.fu-berlin.de>
2012-10-05 15:13:17 +0200

    first

---
+++ src/model/file.py
@@ -0,0 +1,113 @@
+'''
+Created on 20.09.2012
+
+@author: estani
+'''
+import json
+import glob
+
+class BaselineFile(object):
+    BASELINE = [
+        #baseline 0 data      
+        {
+        "root_dir":"/gpfs_750/projects/CMIP5/data/",
+        "parts_dir":"project/product/institute/model/experiment/time_frequency/realm/cmor_table/ensemble/version/variable/file_name".split('/'),
+        "parts_file_name":"variable-cmor_table-model-experiment-ensemble-time".split('-'),
+        "parts_time":"start_time-end_time",
+        "defaults" : {"project":"cmip5", "institute":"MPI-M", "model":"MPI-ESM-LR"}
+        }]
+
+    def __init__(self, file_dict=None, baseline_nr=0):
+        self.baseline_nr = baseline_nr
+        if not file_dict: file_dict = {} 
+        self.dict = file_dict  
+        
+    def __repr__(self):
+        return self.to_json()
+    
+    def to_json(self):
+        return json.dumps(self.dict)
+    
+    def to_path(self):
+        #TODO: check if construction is complete ande therefore can succeed
+        result = [self.dict['root_dir']]
+        for key in BaselineFile._get_baseline(self.baseline_nr)['parts_dir']:
+            result.append(self.dict['parts'][key])
+        return '/'.join(result).replace('//', '/')
+    
+    #a test
+    @staticmethod
+    def from_path(path, baseline_nr=0):
+        
+        bl = BaselineFile._get_baseline(baseline_nr)
+    
+    
+        #trim root_dir
+        if not path.startswith(bl['root_dir']):
+            raise Exception("This file does not correspond to baseline %s" % baseline_nr)                                                         
+        
+        parts = path[len(bl['root_dir']):].split('/')
+        
+        #check the number of parts
+        if len(parts) != len(bl['parts_dir']):
+            raise Exception("Can't parse this path. Expected %d elements but got %d." % (len(bl['parts_dir']), len(parts)))
+        
+        #first the dir
+        result = {}
+        result['root_dir'] = bl['root_dir']
+        result['parts'] = {}
+        for i in range(len(bl['parts_dir'])):
+            result['parts'][bl['parts_dir'][i]] = parts[i]
+        
+        #split file name
+        ##(extract .nc before splitting)
+        parts = result['parts']['file_name'][:-3].split('_')
+        for i in range(len(bl['parts_file_name'])):
+            result['parts'][bl['parts_file_name'][i]] = parts[i]
+        
+        bl_file = BaselineFile(result, baseline_nr=baseline_nr)
+        
+        return bl_file
+
+    @staticmethod
+    def _get_baseline(baseline_nr):
+        """Returns the Object representing the baseline given.
+        Throws an exception if there's no such baseline implemented.
+        (NOTE: baseline refers to the different states of data for comparison in the MiKlip project"""
+        if baseline_nr < 0 or baseline_nr > len(BaselineFile.BASELINE) - 1:
+            raise Exception("Baseline %s not implemented yet." % baseline_nr)
+        return BaselineFile.BASELINE[baseline_nr]
+    
+    @staticmethod
+    def from_dict(file_dict, baseline_nr=0):
+        #need to check file_dict is as expected...
+        #if 'baseline_nr' in file_dict:
+        #    baseline_nr = file_dict['baseline_nr']
+        #    del file_dict['baseline_nr']
+        return BaselineFile(file_dict, baseline_nr=baseline_nr)
+        
+    @staticmethod
+    def from_json(json_str, baseline_nr=0):
+        return BaselineFile.from_dict(json.loads(json_str), baseline_nr=baseline_nr)
+    
+    @staticmethod
+    def search(baseline_nr=0,**partial_dict):
+        """Search for files from the given parameters as part of the baseline names.
+        returns := list of Matching Baseline files"""
+        bl = BaselineFile._get_baseline(baseline_nr)
+        search_dict = bl['defaults'].copy()
+        search_dict.update(partial_dict)
+        
+        result = [bl['root_dir']]
+        for key in BaselineFile._get_baseline(baseline_nr)['parts_dir']:
+            if key in search_dict:
+                result.append(search_dict[key])
+            else:
+                result.append("*")
+        
+        local_path = '/'.join(result).replace('//', '/')
+        bl_files = []
+        for path in glob.glob(local_path):
+            bl_files.append(BaselineFile.from_path(path, baseline_nr))
+        #go to the system and get the list of files
+        return bl_files
---
+++ src/tests/file_test.py
@@ -0,0 +1,62 @@
+'''
+Created on 20.09.2012
+
+@author: estani
+'''
+import unittest
+from model.file import BaselineFile
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.real_path = '/gpfs_750/projects/CMIP5/data/cmip5/output1/MPI-M/MPI-ESM-LR/decadal1960/mon/land/Lmon/r1i1p1/v20111122/c3PftFrac/c3PftFrac_Lmon_MPI-ESM-LR_decadal1960_r1i1p1_196101-199012.nc'
+        self.real_dict = {'parts': {'cmor_table': 'Lmon', 'product': 'output1', 'realm': 'land', 'version': 'v20111122', 'institute': 'MPI-M', 'file_name': 'c3PftFrac_Lmon_MPI-ESM-LR_decadal1960_r1i1p1_196101-199012.nc', 'project': 'cmip5', 'time_frequency': 'mon', 'experiment': 'decadal1960', 'time': '196101-199012', 'variable': 'c3PftFrac', 'model': 'MPI-ESM-LR', 'ensemble': 'r1i1p1'}, 'root_dir': '/gpfs_750/projects/CMIP5/data/'}
+        self.real_json = '{"parts": {"cmor_table": "Lmon", "product": "output1", "realm": "land", "version": "v20111122", "institute": "MPI-M", "file_name": "c3PftFrac_Lmon_MPI-ESM-LR_decadal1960_r1i1p1_196101-199012.nc", "project": "cmip5", "time_frequency": "mon", "experiment": "decadal1960", "time": "196101-199012", "variable": "c3PftFrac", "model": "MPI-ESM-LR", "ensemble": "r1i1p1"}, "root_dir": "/gpfs_750/projects/CMIP5/data/"}'
+        self.baslinefile = BaselineFile(self.real_dict)
+        
+    def tearDown(self):
+        pass
+
+
+    def test_from_path(self):        
+        print "Testing from path with path %s" % self.real_path
+        file_dict = BaselineFile.from_path(self.real_path)
+        #check we have something
+        self.assertNotEqual(file_dict, None)
+        #check we have what we expect
+        self.assertEqual(file_dict.dict, self.real_dict)
+
+    def test_from_dict(self):        
+        print "Testing from dict with dict %s" % self.real_dict
+        file_dict = BaselineFile.from_dict(self.real_dict)
+        #check we have something
+        self.assertNotEqual(file_dict, None)
+        #check we have what we expect
+        self.assertEqual(file_dict.dict, self.real_dict)
+
+    def test_from_json(self):        
+        print "Testing from json with json %s" % self.real_json
+        file_dict = BaselineFile.from_json(self.real_json)
+        #check we have something
+        self.assertNotEqual(file_dict, None)
+        #check we have what we expect
+        self.assertEqual(file_dict.dict, self.real_dict)
+
+    def test_to_path(self):
+        print self.baslinefile
+        path = self.baslinefile.to_path();
+        #check we have something
+        self.assertNotEqual(path, None)
+        #check we have what we expect
+        self.assertEqual(path, self.real_path)
+        
+    def test_search(self):
+        result = BaselineFile.search(experiment='decadal1960', variable='tas')
+        #check we have something
+        self.assertNotEqual(result, None)
+        print "found %s files" % len(result)
+        
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
