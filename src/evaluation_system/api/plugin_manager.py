66c0f11db98e965c1397deda6376f3f137d336c0
estani@tux14 <estanislao.gonzalez@met.fu-berlin.de>
2012-12-07 17:12:17 +0100

    added reloading capabilities

--- src/evaluation_system/api/plugin_manager.py
+++ src/evaluation_system/api/plugin_manager.py
@@ -11,34 +11,48 @@ import sys
 import evaluation_system.api.plugin as plugin
 
 #get the tools directory from the current one
-tools_dir = os.path.join(os.path.abspath(__file__)[:-len('src/evaluation_system/__init__.py')-1],'tools')
+tools_dir = os.path.join(os.path.abspath(__file__)[:-len('src/evaluation_system/api/plugin_manager.py')-1],'tools')
 #all plugins modules will be dynamically loaded here.
 __plugin_modules__ = {}
 """Dictionary of modules holding the plugins"""
 __plugins__ = {}
-"""Dictionary of plugins"""
+"""Dictionary of plugins class_name=>class)"""
+__plugins_meta = {}
+"""Dictionary of plugins with more information 
+plugin_name=>{
+    name=>plugin_name,
+    plugin_class=>class,
+    version=>(0,0,0)
+    description=>"string"}"""
+ 
 
-#get all modules from the tool directory
-for plugin_imp in os.listdir(tools_dir):
-    if not plugin_imp.startswith('.'):
-        #check if api available
-        int_dir = os.path.join(tools_dir,plugin_imp,'integration') 
-        if os.path.isdir(int_dir):
-            #we have a plugin_imp with defined api
-            sys.path.append(int_dir)
-            __plugin_modules__[plugin_imp] = __import__(plugin_imp + '.api')
+def reloadPulgins():
+    #get all modules from the tool directory
+    for plugin_imp in os.listdir(tools_dir):
+        if not plugin_imp.startswith('.'):
+            #check if api available
+            int_dir = os.path.join(tools_dir,plugin_imp,'integration') 
+            if os.path.isdir(int_dir):
+                #we have a plugin_imp with defined api
+                sys.path.append(int_dir)
+                __plugin_modules__[plugin_imp] = __import__(plugin_imp + '.api')
+    
+    #load all plugin classes found (they are loaded when loading the modules)
+    for plug_class in plugin.PluginAbstract.__subclasses__():
+        __plugins__[plug_class.__name__] = plug_class
 
-#load all plugin classes found (they are loaded when loading the modules)
-for plug_class in plugin.PluginAbstract.__subclasses__():
-    __plugins__[plug_class.__name__] = plug_class
-
+    #now fill up the metadata
+    for plugin_name, plugin_class in __plugins__.items():
+        __plugins_meta[plugin_name.lower()] = dict(name=plugin_name,
+                           plugin_class=plugin_class,
+                           version=plugin_class.__version__,
+                           description=plugin_class.__short_description__)
 #This only runs once after start. To load new plugins on the fly we have 2 possibilities
 #1) Watch the tool directory
 #2) Use the plugin metaclass trigger (see `evaluation_system.api.plugin`
-
+reloadPulgins()
 
 from evaluation_system.api import plugin
-from evaluation_system import __plugin_modules__, __plugins__
 
 class PluginManagerException(Exception):
     pass
@@ -48,18 +62,10 @@ def getPulginModules():
     """Return a dictonary with all modules holding the plugins"""
     return __plugin_modules__
 
-__plugins = None
+
 def getPlugins():
     """Return a list of plugin dictionary holding the plugin classes and metadata about them"""
-    global __plugins
-    if __plugins is None:
-        __plugins = {}
-        for plugin_name, plugin_class in __plugins__.items():
-            __plugins[plugin_name.lower()] = dict(name=plugin_name,
-                               plugin_class=plugin_class,
-                               version=plugin_class.__version__,
-                               description=plugin_class.__short_description__)
-    return __plugins
+    return __plugins_meta
 
 def getPlugin(plugin_name):
     """Return the requested plugin or raise an exception if not found."""
--- src/evaluation_system/tests/plugin_manager_test.py
+++ src/evaluation_system/tests/plugin_manager_test.py
@@ -5,8 +5,25 @@ Created on 23.11.2012
 '''
 import unittest
 import evaluation_system.api.plugin_manager as pm
-from evaluation_system.api.plugin import PluginAbstract
+from evaluation_system.api.plugin import PluginAbstract, metadict
+
+class DummyPlugin(PluginAbstract):
+    """Stub class for implementing the abstrac one"""
+    __short_description__ = None
+    __version__ = (0,0,0)
+    _config =  metadict(compact_creation=True, number=(None, dict(type=int)), something='test', other=1.4)
+    _template = "${number} - $something - $other"
+    def runTool(self, config_dict=None):
+        PluginAbstract.runTool(self, config_dict=config_dict)
+    def getHelp(self):
+        PluginAbstract.getHelp(self)
+    def parseArguments(self, opt_arr, default_cfg=None):
+        return PluginAbstract.parseArguments(self, opt_arr, default_cfg=default_cfg)
+    def setupConfiguration(self, config_dict=None, template=None, check_cfg=True):
+        return PluginAbstract.setupConfiguration(self, config_dict=config_dict, template=template, check_cfg=check_cfg)
+    
 class Test(unittest.TestCase):
+    
 
     def testModules(self):
         pmod = pm.getPulginModules()
@@ -14,14 +31,26 @@ class Test(unittest.TestCase):
         self.assertTrue(len(pmod)>0)
 
     def testPlugins(self):
+        #All but the dummy plugin as it should have been created after the module got imported.
         plugins = pm.getPlugins()
-        print plugins
+        old_plug_count = len(plugins)
         
         for key, p in plugins.items():
             for key in ['plugin_class', 'version', 'name', 'description']:
                 self.assertTrue(key in p)
             #Assure the class is derived 
             self.assertTrue(PluginAbstract in p['plugin_class'].__bases__)
+        
+        #now force reload
+        pm.reloadPulgins()
+        self.assertEqual(len(pm.getPlugins()), old_plug_count + 1)
+        self.assertTrue('dummyplugin' in pm.getPlugins())
+        dummy = pm.getPlugin('dummyplugin')
+        self.assertEqual(dummy['description'], DummyPlugin.__short_description__)
+        self.assertEqual(dummy['version'], DummyPlugin.__version__)
+        self.assertEqual(dummy['plugin_class'], DummyPlugin)
+        
+        
 
 
 if __name__ == "__main__":
