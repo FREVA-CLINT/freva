cb94d43ddc03324a588ff6f4ee9ba6d703d3a281
estani@tux14 <estanislao.gonzalez@met.fu-berlin.de>
2012-12-10 13:33:59 +0100

    add writeConfig and runTool

--- src/evaluation_system/api/plugin_manager.py
+++ src/evaluation_system/api/plugin_manager.py
@@ -9,6 +9,8 @@ Created on 23.11.2012
 import os
 import sys
 import evaluation_system.api.plugin as plugin
+import logging
+log = logging.getLogger(__name__)
 
 #get the tools directory from the current one
 tools_dir = os.path.join(os.path.abspath(__file__)[:-len('src/evaluation_system/api/plugin_manager.py')-1],'tools')
@@ -52,7 +54,7 @@ def reloadPulgins():
 #2) Use the plugin metaclass trigger (see `evaluation_system.api.plugin`
 reloadPulgins()
 
-from evaluation_system.api import plugin
+from evaluation_system.model.user import User
 
 class PluginManagerException(Exception):
     pass
@@ -68,10 +70,67 @@ def getPlugins():
     return __plugins_meta
 
 def getPlugin(plugin_name):
-    """Return the requested plugin or raise an exception if not found."""
+    """Return the requested plugin dictionary entry or raise an exception if not found.
+    Parameters
+    plugin_name:=string
+        Name of the plugin to search for.
+    @return: a dictionary with information on the plugin 
+            {name=>plugin_name,
+            plugin_class=>class,
+            version=>(0,0,0)
+            description=>"string"}"""
     plugin_name = plugin_name.lower()
     if plugin_name not in getPlugins(): raise PluginManagerException("No plugin named: %s" % plugin_name)
     
     return getPlugins()[plugin_name]
+
+def writeSetup(plugin_name, config_dict=None, user=None):
+    """Writes the plugin setup to disk. This is the configuration for the plugin itself and not that
+    of the tool. The plugin might not write anything to disk when running the tool and instead configure it
+    from the command line, environmental variables or any other method.
+    Parameters
+    plugin_name:=name of the refered plugin (mandatory)
+    config_dict:=config dict or metadict for seting up the configuration
+        if None, the default configuration will be stored, this might be incomplete and thus might
+        not be enough for triggereing the plugin.
+    user:=`evaluation_system.model.user.User`
+        The user for whom this will be run. If None, then the current user will be used.
+    @return: The path to the written configuration file."""
+    plugin_name = plugin_name.lower()
+    if user is None:
+        user = User()
+    #make sure the required directory structure and data is in place
+    user.prepareDir()
+    
+    conf_dir = user.getUserConfigDir(plugin_name, create=True)
+
+    p = getPlugin(plugin_name)['plugin_class']()
+    complete_conf = p.setupConfiguration(config_dict=config_dict, check_cfg=False) 
+    conf_file = os.path.join(conf_dir,'%s.conf' % plugin_name)
+    with open(conf_file, 'w') as f:
+        p.saveConfiguration(f, config_dict=complete_conf)
+    
+    return conf_file
+
+def runTool(plugin_name, config_dict=None, user=None):
+    plugin_name = plugin_name.lower()
+    if user is None:
+        user = User()
+    p = getPlugin(plugin_name)['plugin_class']()
+    complete_conf = None
+    if config_dict is None:
+        conf_dir = user.getUserConfigDir(plugin_name)
+        conf_file = os.path.join(conf_dir,'%s.conf' % plugin_name)
+        if os.path.isfile(conf_file):
+            log.debug('Loading config file %s', conf_file)
+            with open(conf_file, 'r') as f:
+                complete_conf = p.readConfiguration(f)
+        else:
+            log.debug('No config file was found in %s', conf_file)
+    if complete_conf is None:
+        complete_conf = p.setupConfiguration(config_dict=config_dict, check_cfg=False) 
+    
+    #In any case we have now a complete setup in complete_conf
+    p.runTool(config_dict=complete_conf)
 
 
--- src/evaluation_system/tests/plugin_manager_test.py
+++ src/evaluation_system/tests/plugin_manager_test.py
@@ -4,24 +4,59 @@ Created on 23.11.2012
 @author: estani
 '''
 import unittest
+import os
+import tempfile
 import evaluation_system.api.plugin_manager as pm
 from evaluation_system.api.plugin import PluginAbstract, metadict
+from evaluation_system.model.user import User
+import shutil
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 class DummyPlugin(PluginAbstract):
     """Stub class for implementing the abstrac one"""
     __short_description__ = None
     __version__ = (0,0,0)
-    _config =  metadict(compact_creation=True, number=(None, dict(type=int)), something='test', other=1.4)
+    __config_metadict__ =  metadict(compact_creation=True, 
+                                    number=(None, dict(type=int,help='This is just a number, not really important')),
+                                    the_number=(None, dict(type=int,mandatory=True,help='This is *THE* number. Please provide it')), 
+                                    something='test', other=1.4)
+    _runs = []
     _template = "${number} - $something - $other"
     def runTool(self, config_dict=None):
-        PluginAbstract.runTool(self, config_dict=config_dict)
-    def getHelp(self):
-        PluginAbstract.getHelp(self)
-    def parseArguments(self, opt_arr, default_cfg=None):
-        return PluginAbstract.parseArguments(self, opt_arr, default_cfg=default_cfg)
-    def setupConfiguration(self, config_dict=None, template=None, check_cfg=True):
-        return PluginAbstract.setupConfiguration(self, config_dict=config_dict, template=template, check_cfg=check_cfg)
-    
+        DummyPlugin._runs.append(config_dict)
+        print "Dummy tool was run with: %s" % config_dict
+        
+
+class DummyUser(User):
+    """Create a dummy User object that allows testing"""
+    def __init__(self, random_home=False, **override):
+        if random_home:
+            if 'pw_dir' in override:
+                raise Exception("Can't define random_home and provide a home directory")
+            override['pw_dir'] = tempfile.mkdtemp('_es_userdir')
+            
+        User.__init__(self)
+        
+        class DummyUserData(list):
+            """Override a normal list and make it work like the pwd read-only struct"""
+            _NAMES = 'pw_name pw_passwd pw_uid pw_gid pw_gecos pw_dir pw_shell'.split()
+            def __init__(self, arr_list):
+                list.__init__(self, arr_list)
+            def __getattribute__(self, name):
+                #don't access any internal variable (avoid recursion!)
+                if name[0] != '_' and name in self._NAMES:
+                    return self[self._NAMES.index(name)]
+                return list.__getattribute__(self, name)
+                    
+        
+        #copy the current data
+        user_data = list(self._userdata[:])
+        for key, value in override.items():
+            if key in DummyUserData._NAMES:
+                user_data[DummyUserData._NAMES.index(key)] = value
+        self._userdata = DummyUserData(user_data)
+        
 class Test(unittest.TestCase):
     
 
@@ -31,27 +66,87 @@ class Test(unittest.TestCase):
         self.assertTrue(len(pmod)>0)
 
     def testPlugins(self):
-        #All but the dummy plugin as it should have been created after the module got imported.
-        plugins = pm.getPlugins()
-        old_plug_count = len(plugins)
-        
-        for key, p in plugins.items():
-            for key in ['plugin_class', 'version', 'name', 'description']:
-                self.assertTrue(key in p)
-            #Assure the class is derived 
-            self.assertTrue(PluginAbstract in p['plugin_class'].__bases__)
-        
-        #now force reload
+        #force reload to be sure the dummy is loaded
         pm.reloadPulgins()
-        self.assertEqual(len(pm.getPlugins()), old_plug_count + 1)
+        self.assertTrue(len(pm.getPlugins())> 0)
         self.assertTrue('dummyplugin' in pm.getPlugins())
         dummy = pm.getPlugin('dummyplugin')
         self.assertEqual(dummy['description'], DummyPlugin.__short_description__)
         self.assertEqual(dummy['version'], DummyPlugin.__version__)
         self.assertEqual(dummy['plugin_class'], DummyPlugin)
         
+    def testDefaultPluginConfigStorage(self):
+        user = DummyUser(random_home=True, pw_name='test_user')
+        home = user.getUserHome()
+        self.assertTrue(os.path.isdir(home))
+        pm.reloadPulgins()
+        conf_file = pm.writeSetup('dummyplugin', user=user)
         
+        print conf_file
+        self.assertTrue(os.path.isfile(conf_file))
+        with open(conf_file, 'r')as f:
+            print f.read()
+        
+        if os.path.isdir(home) and home.startswith(tempfile.gettempdir()):
+            #make sure the home is a temporary one!!!
+            print "Cleaning up %s" % home
+            shutil.rmtree(home)
+            
+    def testPluginConfigStorage(self):
+        user = DummyUser(random_home=True, pw_name='test_user')
+        home = user.getUserHome()
+        self.assertTrue(os.path.isdir(home))
+        pm.reloadPulgins()
+        conf_file = pm.writeSetup('dummyplugin', config_dict=dict(the_number=42),user=user)
+        
+        print conf_file
+        self.assertTrue(os.path.isfile(conf_file))
+        with open(conf_file, 'r')as f:
+            print f.read()
+            
+        #conf_file = pm.readSetup('dummyplugin', config_dict=dict(number=1234),user=user)
+        
+        if os.path.isdir(home) and home.startswith(tempfile.gettempdir()):
+            #make sure the home is a temporary one!!!
+            print "Cleaning up %s" % home
+            shutil.rmtree(home)
+            
+    def testRun(self):
+        pm.reloadPulgins()
+        user = DummyUser(random_home=True, pw_name='test_user')
+        home = user.getUserHome()
+        
+        #no confg
+        pm.runTool('dummyplugin', user=user)
+        self.assertTrue(len(DummyPlugin._runs) == 1)
+        run = DummyPlugin._runs[0]
+        self.assertTrue('the_number' in run)
+        self.assertTrue(run['the_number'] is None)
+        DummyPlugin._runs = []
+        
+        #direct config
+        pm.runTool('dummyplugin', user=user, config_dict=dict(the_number=42))
+        self.assertTrue(len(DummyPlugin._runs) == 1)
+        run = DummyPlugin._runs[0]
+        self.assertTrue('the_number' in run)
+        self.assertTrue(run['the_number'] == 42)
+        DummyPlugin._runs = []
+        
+        #config stored on disk
+        cf = pm.writeSetup('DummyPlugin', dict(the_number=777), user)
+        self.assertTrue(os.path.isfile(cf))
+        print cf
+        pm.runTool('dummyplugin', user=user)
+        self.assertTrue(len(DummyPlugin._runs) == 1)
+        run = DummyPlugin._runs[0]
+        self.assertTrue('the_number' in run)
+        self.assertTrue(run['the_number'] == 777)
+        DummyPlugin._runs = []
 
+        if os.path.isdir(home) and home.startswith(tempfile.gettempdir()):
+            #make sure the home is a temporary one!!!
+            print "Cleaning up %s" % home
+            shutil.rmtree(home)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
