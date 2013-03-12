41fa609962ab9b30d470b489eeb1c29de0dfef8e
estani@tux14 <estanislao.gonzalez@met.fu-berlin.de>
2013-03-12 19:12:42 +0100

    add: solr dump/load from csv

--- src/evaluation_system/model/solr.py
+++ src/evaluation_system/model/solr.py
@@ -7,10 +7,13 @@ This package encapsulate access to a solr instance
 '''
 
 import os
-import urllib2
+import urllib2, urllib
 import json
 
 from evaluation_system.model.file import DRSFile, BASELINE0, BASELINE1, CMIP5, OBSERVATIONS, REANALYSIS
+
+
+
 
 class SolrFindFiles(object):
     """Encapsulate access to Solr for the find files command"""
@@ -33,7 +36,7 @@ class SolrFindFiles(object):
     
     def get_json(self, endpoint):
         """Return some json from server"""
-        req=urllib2.Request(self.solr_url + endpoint)    
+        req=urllib2.Request(self.core_url + endpoint)    
         
         return json.loads(urllib2.urlopen(req).read())
     
@@ -43,18 +46,18 @@ class SolrFindFiles(object):
             self._fields = self.get_json(self.core_url + '/admin/luke?wt=json')['fields']
         return self._fields
     
-    def wipe_out(self):
+    def delete(self, query='*:*'):
         """Wipes out the complete Solr index"""
-        self.post(dict(delete=dict(query="*:*")), auto_list=False)
+        self.post(dict(delete=dict(query=query)), auto_list=False)
     
-    def update(self, data_types=None, processors=5):
+    def update(self, data_types=None, processors=1, batch_size=1000):
         """Updated the Solr index, by ingesting every file in to it"""
         if data_types is None:
             data_types = [REANALYSIS, OBSERVATIONS, BASELINE0, BASELINE1, CMIP5]
 
         from multiprocessing import Queue, Process
-        q = Queue(1000)
-        handle_file_init(q, batch_size=500)
+        q = Queue(processors * batch_size)  #just store one extra batch load for every processor
+        handle_file_init(q, batch_size=batch_size)
         end_token = '*END-OF-QUEUE*'
         p = Process(target=find_files, args=(q,data_types,))
         p.start()
@@ -72,7 +75,106 @@ class SolrFindFiles(object):
         for i in range(processors):
             procs[i].join()
 
+    def to_solr_query(self, partial_dict):
+        params = []
+        for key, value in partial_dict.items():
+            if key.endswith('_not_'):
+                #handle negation
+                key = '-' + key[:-5]
+            if isinstance(value, list):
+                #implies an or
+                constraint = ' OR '.join(['%s:%s' % (key,v) for v in value])
+            else:
+                constraint = '%s:%s' % (key, value)
+            params.append(('fq', constraint,))
+        return urllib.urlencode(params)
+        
 
+    def _search(self, **partial_dict):
+        batch_size = 100
+        offset = 0
+        query = SolrFindFiles.to_solr_query(partial_dict)
+        while True:
+            answer = self.get_json('/select?wt=json&fl=file&start=%s&rows=%s&%s' % (offset, batch_size, query))
+            offset = answer['response']['start']
+            total = answer['response']['numFound']
+            for item in answer['response']['docs']:
+                yield item['file']
+            
+            if total - offset <= batch_size:
+                break   #we are done
+            else:
+                offset += batch_size
+        
+    @staticmethod
+    def search (**partial_dict):
+        #use default
+        s = SolrFindFiles()
+        return s._search(partial_dict)
+
+    def dump(self, dump_file=None, batch_size=1000):
+        if dump_file is None:
+            from datetime import datetime
+            #just to store where and how we are storing this
+            dump_file = datetime.now().strftime('/miklip/integration/infrastructure/solr/backup_data/%Y%m%d.csv')
+
+        def cache(batch_size):
+            offset = 0
+            while True:
+                url_query = '/select?wt=json&fl=file,timestamp&start=%s&rows=%s&sort=file+desc&q=*' % (offset, batch_size)
+                print "Calling %s" % url_query
+                answer = self.get_json(url_query)
+                offset = answer['response']['start']
+                total = answer['response']['numFound']
+                for item in answer['response']['docs']:
+                    yield (item['file'], item['timestamp'],)
+                if total - offset <= batch_size:
+                    break   #we are done
+                else:
+                    offset += batch_size
+
+        with open(dump_file, 'w') as f:
+            for file_path, timestamp in cache(batch_size=batch_size):
+                f.write('%s,%s\n' % (file_path, timestamp))
+
+    def load(self, dump_file=None, batch_size=1000):
+        if dump_file is None:
+            from datetime import datetime
+            dump_file = datetime.now().strftime('/miklip/integration/infrastructure/solr/backup_data/%Y%m%d.csv')
+
+        batch_count=0
+        batch = []
+        with open(dump_file, 'r') as f:
+            for file_path, timestamp in (line.split(',') for line in f):
+                try:
+                    data_type = DRSFile.find_structure_from_path(file_path)
+                    drs_file = DRSFile.from_path(file_path, drs_structure=data_type)
+                    metadata = drs_file.dict['parts'].copy()
+                    metadata['file'] = file_path
+                    metadata['data_type'] = data_type
+                    metadata['timestamp'] = float(timestamp)
+
+                    batch.append(metadata)
+
+                    if len(batch) >= batch_size:
+                        print "Sending entries %s-%s" % (batch_count * batch_size, (batch_count+1) * batch_size)
+                        self.post(batch)
+                        batch = []
+                        batch_count += 1
+
+                except:
+                    print "Can't ingest file %s" % file_path
+
+        #flush the batch queue
+        if batch:
+            print "Sending last %s entries." % (len(batch))
+            self.post(batch)
+
+#-- These are for multiple processes... 
+#but There's no benefit for having multiple threads at this time
+#on the contrary, it's worse :-/
+#There's no improvement for not having this construct either, so I'm leaving it
+#here. It might help in the future...
 def find_files(q, data_types):
     if not isinstance(data_types, list): 
         data_types = [ data_types]
@@ -81,6 +183,7 @@ def find_files(q, data_types):
             metadata = nc_file.dict['parts'].copy()
             metadata['data_type'] = data_type
             metadata['file'] = nc_file.to_path()
+            metadata['dataset'] = nc_file.to_dataset()
             #don't do this now, this takes a while
             #metadata['timestamp'] = os.path.getmtime(nc_file.to_path())
             q.put(metadata)
@@ -100,6 +203,9 @@ def handle_file(number, end_token):
         if value == end_token:
             handle_file.q.put(end_token)
             break
+        #import scipy.io.netcdf
+        #with scipy.io.netcdf.netcdf_file(value['file'], 'r') as f:
+        #    value.update(f._attributes)
         value['timestamp'] = os.path.getmtime(value['file'])
         batch.append(value)
         if len(batch) >= handle_file.batch_size:
