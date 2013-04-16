26e454419f7033a93f4e5657ce5a266cc1a05ab1
estani@tux14 <estanislao.gonzalez@met.fu-berlin.de>
2013-04-16 17:19:55 +0200

    add: some info about autocomplete and extended it to options

--- etc/bash_3/solr.sh
+++ etc/bash_3/solr.sh
@@ -1,7 +1,7 @@
 _solr_query() {
     local query 
     for elem in "$@"; do
-        [[ "$elem" == "-*" ]] && continue
+        [[ "${elem:0:1}" == "-" ]] || grep -qv =<<<"$elem" && continue
         query="$query $elem"
     done    
     echo "$query"
@@ -12,17 +12,28 @@ _solr_search_show() {
     cur="${COMP_WORDS[COMP_CWORD]}"
     query="$(_solr_query "${COMP_WORDS[@]:1:COMP_CWORD-1}")"
     case "$cur" in
+    -*)
+        opts="--all-facets --attributes --baseline --baseline0 --baseline1 --batch-size --cmip5 --count-facet-values -d --debug --facet -h --help --multiversion --observations --reanalysis"
+        COMPREPLY=( $(compgen -W "${opts}" -- "$cur") )
+        ;;
     *=)
         opts="$(solr_search --facet "${cur/=}" $query | sed -e 's/[^:]*: //' -e 's/,/ /g')"
         COMPREPLY=( $(compgen -W "${opts}") )
         ;;
     *=*)
         opts="$(solr_search --facet "${cur/=*}" $query ${cur}* | sed -e 's/[^:]*: //' -e 's/,/ /g')"
-        COMPREPLY=( $(compgen -W "${opts}" "${cur#*=}") )
+        COMPREPLY=( $(compgen -W "${opts}" -- "${cur#*=}") )
         ;;
     *)
-        opts="$(solr_search --attributes $query | sed  -e 's/,/=/g')"
-        COMPREPLY=( $(compgen -W "${opts}" "$cur") )
+        prev="${COMP_WORDS[COMP_CWORD-1]}"
+        if [[ "$prev" == "--facet" ]]; then
+            opts="$(solr_search --attributes $query | sed  -e 's/,//g')"
+        elif [[ "$prev" == "--baseline" ]]; then
+            opts="0 1"
+        else
+            opts="$(solr_search --attributes $query | sed  -e 's/,/=/g')="
+        fi
+        COMPREPLY=( $(compgen -W "${opts}" -- "$cur") )
     esac
 }
 complete -o nospace -F _solr_search_show solr_search
