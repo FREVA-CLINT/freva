c39f1e70b3fa8a069a900d71d80425f363e42d61
estani@miklip <estanislao.gonzalez@met.fu-berlin.de>
2013-04-18 11:24:56 +0200

    chg: updated modules

--- etc/autocomplete.bash
+++ etc/autocomplete.bash
@@ -2,7 +2,7 @@
 
 etc_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "${BASH_VERSION:0:1}" == "3" ]]; then
-    . $etc_dir/bash_3/* && echo Successfully loaded scripts for bash 3.x
+    . $etc_dir/bash_3/* && echo bash 3.x auto-completion script succesfully loaded
 elif [[ "${BASH_VERSION:0:1}" == "4" ]]; then
-    . $etc_dir/bash_3/* && echo Successfully sourced scripts for bash 4.x
+    . $etc_dir/bash_3/* && echo bash 4.x auto-completion script succesfully loaded
 fi
