36286be134256a631cacb037e224a4fcc82e2cff
Christopher Kadow <christopher.kadow@met.fu-berlin.de>
2013-10-08 10:29:23 +0200

    added DSI tool, written into etc/evaluation_system.conf

--- etc/autocomplete.bash
+++ etc/autocomplete.bash
@@ -2,7 +2,7 @@
 
 etc_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "${BASH_VERSION:0:1}" == "3" ]]; then
-    . $etc_dir/bash_3/* && echo bash 3.x auto-completion script succesfully loaded
+    . $etc_dir/bash_3/* && echo bash 3.x auto-completion script successfully loaded
 elif [[ "${BASH_VERSION:0:1}" == "4" ]]; then
-    . $etc_dir/bash_4/* && echo bash 4.x auto-completion script succesfully loaded
+    . $etc_dir/bash_4/* && echo bash 4.x auto-completion script successfully loaded
 fi
