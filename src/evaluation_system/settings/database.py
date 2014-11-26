ae00cb6925f9396eb0842385bc8b8fc309c5440e
kunst <oliver.kunst@met.fu-berlin.de>
2014-11-26 15:41:16 +0100

    Settings for miklip

--- src/evaluation_system/settings/database.py
+++ src/evaluation_system/settings/database.py
@@ -6,8 +6,8 @@ SETTINGS = dict()
 SETTINGS['DATABASES'] = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'evaluationsystemtest',
-        'USER': 'evaluationsystemtest',
+        'NAME': 'evaluationsystem',
+        'USER': 'evaluationsystem',
         'PASSWORD': 'miklip',
         'HOST': '136.172.30.208',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
