87a5739a65aa37dc036d34d079f3bbce360e43bb
Oliver Kunst <u290038@wwwdev-miklip.dkrz.de>
2014-12-01 15:08:45 +0100

    bigger try block avoids web page problems, but could cause trouble...

--- src/evaluation_system/settings/database.py
+++ src/evaluation_system/settings/database.py
@@ -4,19 +4,19 @@ from evaluation_system.misc import config
 import json,time
 
 SETTINGS = dict()
-SETTINGS['DATABASES'] = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config.get(config.DB_DB),
-        'USER': config.get(config.DB_USER),
-        'PASSWORD': config.get(config.DB_PASSWD),
-        'HOST': config.get(config.DB_HOST),   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-
-    }
-}
 
 try:
+    SETTINGS['DATABASES'] = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': config.get(config.DB_DB),
+            'USER': config.get(config.DB_USER),
+            'PASSWORD': config.get(config.DB_PASSWD),
+            'HOST': config.get(config.DB_HOST),   # Or an IP Address that your DB is hosted on
+            'PORT': '3306',
+
+        }
+    }
     settings.configure(**SETTINGS)
 except:
     pass
