7d3bf5b4c4b99ba60c965c0e15c21575079f888d
Thomas Schartner <b324029@miklip11.dkrz.de>
2015-05-12 15:27:37 +0200

    - added esgf_crawl - added esgf2solr_crawl - added esgf_crawl_config 	new file:   esgf_crawl 	new file:   ../src/evaluation_system/fuse/__init__.py 	new file:   ../src/evaluation_system/fuse/esgf2solr_crawl.py 	new file:   ../src/evaluation_system/fuse/esgf_crawl_config.py

---
+++ src/evaluation_system/fuse/__init__.py
@@ -0,0 +1,1 @@
+from .esgf2solr_crawl import Esgf2Solr
---
+++ src/evaluation_system/fuse/esgf_crawl_config.py
@@ -0,0 +1,45 @@
+import re
+
+class Esgf2SolrConfig(object):
+    def cmip5(self,dataset):
+        
+        cmip5_structure = dict(
+        filename        = dataset['title'],
+        project         = dataset['project'][0],
+        product         = dataset['product'][0],
+        institute       = dataset['institute'][0],
+        model           = re.sub(r"\)","",re.sub(r"[\.\(\,]","-",dataset['model'][0])),
+        experiment      = dataset['experiment'][0],
+        time_frequency  = dataset['time_frequency'][0],
+        realm           = dataset['realm'][0],
+        cmor_var        = [i for i in dataset['variable'] if i+'_' in filename][0],
+        ensemble        = dataset['ensemble'][0])
+        
+        return cmip5_structure
+    
+    def specs(self,dataset):
+        filename        = dataset['title']
+        year            = str(int(re.compile(r'_S(?P<year>\d{4})\d{2}\d{2}_'+\
+                                  dataset['ensemble'][0]).search(filename).group('year'))-1)
+        
+        specs_structure = dict(
+        filename       = re.sub(dataset['experiment'][0]+'_S\d{8}_'+dataset['ensemble'][0],\
+                                dataset['experiment'][0]+year+'_'+dataset['ensemble'][0],filename),
+        project        = dataset['project'][0],
+        product        = dataset['product'][0],
+        institute      = dataset['institute'][0],
+        model          = re.sub(r"\)","",re.sub(r"[\.\(\,]","-",dataset['model'][0])),
+        experiment     = dataset['experiment'][0]+year,
+        time_frequency = dataset['time_frequency'][0],
+        realm          = dataset['realm'][0],
+        cmor_var       = [i for i in dataset['variable'] if i+'_' in filename][0],
+        ensemble       = dataset['ensemble'][0])
+        
+        return specs_structure
+    
+    def project_select(self,dataset):
+        options = {'cmip5' : self.cmip5,
+                   'specs' : self.specs,
+                   }
+        return options[dataset['project'][0]](dataset)
+    
