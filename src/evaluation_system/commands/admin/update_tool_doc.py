e691fad591240cc240f7ce00cc21bac44b1c36ab
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2015-11-30 12:35:42 +0100

    copy images to preview path / remove tmp files

--- src/evaluation_system/commands/admin/update_tool_doc.py
+++ src/evaluation_system/commands/admin/update_tool_doc.py
@@ -10,6 +10,7 @@ update_tool_doc -- update the html code of tools
 
 from evaluation_system.commands import (BaseCommand, CommandError,
                                         FrevaBaseCommand)
+from evaluation_system.misc import config
 import logging, sys
 import os
 import shutil
@@ -67,7 +68,6 @@ class Command(FrevaBaseCommand):
         fi = open(os.path.join(new_path, file_root+'.html'))
         text = fi.read()
         text = re.sub("<head>.*?</head>", "", text, flags=re.DOTALL)
-        # TODO: should probably be don with regex
         text = text.replace('</html>', '')
         text = text.replace('<html>', '')
         text = text.replace('</body>', '')
@@ -75,15 +75,24 @@ class Command(FrevaBaseCommand):
         
         # replace img src
         text = text.replace('src="figures/',
-                            'style="width:80%;" src="/static/doc/'+tool+'/')
+                            'style="width:80%;" src="/static/preview/doc/'+tool+'/')
+        # remove too big sigma symbols
+        text = text.replace('mathsize="big"', '')
         
         flat_page, created = FlatPage.objects.get_or_create(
             title=self.args.tool, url='/about/%s/' % tool
         )
-        flat_page.sites = [1]
-        # flat_page = FlatPage.objects.get(title__iexact=tool)
+        if created:
+            flat_page.sites = [1]
         flat_page.content = text
         flat_page.save()
+        
+        # Copy images to website preview path
+        preview_path = config.get('preview_path')
+        print preview_path
+        self.copy_and_overwrite('figures/', os.path.join(preview_path, 'doc/%s/' % tool))
+        # remove tmp files
+        shutil.rmtree(new_path)
         
         
 if __name__ == "__main__":
