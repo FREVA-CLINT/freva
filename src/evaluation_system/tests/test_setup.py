4d235f3a265e435ded1580218e2efd1555efe849
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2016-05-23 11:49:50 +0200

    Added tests for history models PEP8 history/models.py

---
+++ src/evaluation_system/tests/test_setup.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from evaluation_system.misc import config
+
+import json,time
+import django
+
+SETTINGS = dict()
+
+try:
+    # Application definition
+    SETTINGS['INSTALLED_APPS'] = (
+        'django.contrib.auth',  # We need this to access user groups
+        'django.contrib.flatpages'
+    )
+    SETTINGS['DATABASES'] = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/var/autofs/net/home/illing/workspace/evaluation_system/src/evaluation_system/tests/local.db'
+        }
+    }
+    settings.configure(**SETTINGS)
+    django.setup()
+except:
+    pass
