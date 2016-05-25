9d490c8f35373a204974a6ff5116132bab48b0e4
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2016-05-25 17:16:37 +0200

    Added tests for databrowser command PEP8 databrowser

--- src/evaluation_system/commands/databrowser.py
+++ src/evaluation_system/commands/databrowser.py
@@ -1,33 +1,41 @@
 # encoding: utf-8
 
-'''
+"""
 databrowser - Find data
 
 @copyright:  2015 FU Berlin. All rights reserved.
         
 @contact:    sebastian.illing@met.fu-berlin.de
-'''
+"""
 
-import logging,sys
-from evaluation_system.commands import FrevaBaseCommand
+import logging
+import sys
+from evaluation_system.commands import FrevaBaseCommand, CommandError
 from evaluation_system.model.solr import SolrFindFiles
+
 
 class Command(FrevaBaseCommand):
         
     _args = [
-             {'name':'--debug','short':'-d','help':'turn on debugging info and show stack trace on exceptions.','action':'store_true'},
-             {'name':'--help','short':'-h', 'help':'show this help message and exit','action':'store_true'},
-             {'name':'--multiversion','help':'select not only the latest version but all of them','action':'store_true', 'default':False},
-             {'name':'--relevant-only','help':'show only facets that filter results (i.e. >1 possible values)','action':'store_true'},
-             {'name':'--batch-size','help':'Number of files to retrieve', 'type':'int', 'metavar':'N'},
-             {'name':'--count-facet-values','help':'Show the number of files for each values in each facet','action':'store_true'},
-             {'name':'--attributes','help':'retrieve all possible attributes for the current search instead of the files','action':'store_true'},
-             {'name':'--all-facets','help':'retrieve all facets (attributes & values) instead of the files','action':'store_true'},
-             {'name':'--facet','help':'retrieve these facets (attributes & values) instead of the files'},
+             {'name': '--debug', 'short': '-d', 'help': 'turn on debugging info and show stack trace on exceptions.',
+              'action': 'store_true'},
+             {'name': '--help', 'short': '-h', 'help': 'show this help message and exit', 'action': 'store_true'},
+             {'name': '--multiversion', 'help': 'select not only the latest version but all of them',
+              'action': 'store_true', 'default': False},
+             {'name': '--relevant-only', 'help': 'show only facets that filter results (i.e. >1 possible values)',
+              'action': 'store_true'},
+             {'name': '--batch-size', 'help': 'Number of files to retrieve', 'type': 'int', 'metavar': 'N'},
+             {'name': '--count-facet-values', 'help': 'Show the number of files for each values in each facet',
+              'action': 'store_true'},
+             {'name': '--attributes', 'help': 'retrieve all possible attributes for the current search instead of the files',
+              'action': 'store_true'},
+             {'name': '--all-facets', 'help': 'retrieve all facets (attributes & values) instead of the files',
+              'action': 'store_true'},
+             {'name': '--facet', 'help': 'retrieve these facets (attributes & values) instead of the files'},
              ] 
     
     __description__ = """
-The query is of the form key=value. <value> might use *, ? as wildcards or any regular expression encclosed in forward slashes. Depending on your shell and the symbols used, remeber to escape the sequences properly. 
+The query is of the form key=value. <value> might use *, ? as wildcards or any regular expression enclosed in forward slashes. Depending on your shell and the symbols used, remember to escape the sequences properly.
 The safest would be to enclosed those in single quotes.
 
 For Example:
@@ -38,9 +46,9 @@ For Example:
     def _run(self):
         
         args = self.args 
-        lastargs = self.last_args 
+        last_args = self.last_args
         
-        #Are we searching for facets or files?
+        # Are we searching for facets or files?
         facets = []
         if args.all_facets:
             facets = None
@@ -51,8 +59,8 @@ For Example:
         batch_size = args.batch_size if args.batch_size else 10
         
         search_dict = {}
-        #contruct search_dict by looping over lastargs
-        for arg in lastargs:
+        # contruct search_dict by looping over last_args
+        for arg in last_args:
             if '=' not in arg:
                 raise CommandError("Invalid format for query: %s" % arg)
             
@@ -67,33 +75,32 @@ For Example:
                 search_dict[key].append(value)
         
         if 'version' in search_dict and latest:
-            #it makes no sense to look for a specific version just among the latest
-            #the speedup is marginal and it might not be what the user expects
+            # it makes no sense to look for a specific version just among the latest
+            # the speedup is marginal and it might not be what the user expects
             sys.stderr.write('Turning latest of when searching for a specific version.')
             latest = False
             
         logging.debug("Searching dictionary: %s\n", search_dict)
-        #exit()
-        #flush stderr in case we have something pending
+        # exit()
+        # flush stderr in case we have something pending
         sys.stderr.flush()
 
         if facets != [] and not args.attributes:
             if 'facet.limit' in search_dict:
                 facet_limit = int(search_dict['facet.limit'])
             else:  
-                #default
+                # default
                 facet_limit = 1000
                 search_dict['facet.limit'] = -1
                 
-            for att,values in SolrFindFiles.facets(facets=facets, latest_version=latest, **search_dict).items():
-                
-                
-                #values come in pairs: (value, count)
+            for att, values in SolrFindFiles.facets(facets=facets, latest_version=latest, **search_dict).items():
+                # values come in pairs: (value, count)
                 value_count = len(values)/2
-                if args.relevant_only and value_count < 2: continue
+                if args.relevant_only and value_count < 2:
+                    continue
                 
                 if args.count_facet_values:
-                    sys.stdout.write('%s: %s' % (att, ','.join(['%s (%s)' % (v,c) for v,c in zip(*[iter(values)]*2)])))
+                    sys.stdout.write('%s: %s' % (att, ','.join(['%s (%s)' % (v, c) for v, c in zip(*[iter(values)]*2)])))
                 else:
                     sys.stdout.write('%s: %s' % (att, ','.join(values[::2])))
                 
@@ -103,8 +110,9 @@ For Example:
                 sys.stdout.write('\n')
                 sys.stdout.flush()
         elif args.attributes:
-            #select all is none defined but this flag was set
-            if facets == []: facets = None
+            # select all is none defined but this flag was set
+            if not facets:
+                facets = None
             results = SolrFindFiles.facets(facets=facets, latest_version=latest, **search_dict)
             if args.relevant_only:
                 atts = ', '.join([k for k in results if len(results[k]) > 2])
@@ -114,18 +122,11 @@ For Example:
             sys.stdout.write('\n')
             sys.stdout.flush()
         else:
-            #find the files and display them
+            # find the files and display them
             for f in SolrFindFiles.search(batch_size=batch_size, latest_version=latest, **search_dict):
                 sys.stdout.write(str(f))
                 sys.stdout.write('\n')
                 sys.stdout.flush()
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma nocover
     Command().run()
-
-
-
-
-
-
-
