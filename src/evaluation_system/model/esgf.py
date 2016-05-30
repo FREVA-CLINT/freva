8980a1a9266e12d7219cff9babe0d2fab981a6b2
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2016-05-30 17:22:53 +0200

    Added tests for esgf command PEP8 esgf

--- src/evaluation_system/model/esgf.py
+++ src/evaluation_system/model/esgf.py
@@ -7,19 +7,17 @@ import urllib2
 import sys
 import logging
 import copy
+from evaluation_system.misc.utils import Struct
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-from evaluation_system.misc.utils import Struct
-
-
-
-
 
 class P2P(object):
-    """Handle the connection to a P2P over the Search API (check the `Search API documentation <http://esgf.org/wiki/ESGF_Search_API>`_."""
-
-    TYPE = Struct.from_dict({'DATASET':'Dataset','FILE':'File'})
+    """
+    Handle the connection to a P2P over the Search API
+    (check the `Search API documentation <http://esgf.org/wiki/ESGF_Search_API>`_.
+    """
+    TYPE = Struct.from_dict({'DATASET': 'Dataset', 'FILE': 'File'})
     """Types of results that will be returned."""
     
     __MAX_RESULTS = 1000
@@ -28,7 +26,6 @@ class P2P(object):
     _time_out = 30
     """Specifies the time out (seconds) when trying to reach an index node."""
 
-
     def __init__(self, node='esgf-data.dkrz.de', api='esg-search/search', wget_api='esg-search/wget', defaults=None):
         """Creates a connection to the P2P search API.
 
@@ -36,17 +33,18 @@ class P2P(object):
 :type node: str
 :param api: the url path of the service.
 :type api: str
-:param defaults: start the connection with the given defaultsused for every search.
+:param defaults: start the connection with the given defaults used for every search.
 :type defaults: dict
-	"""
+        """
         self.node = node
         self.api = api
         self.wget_api = wget_api
         self.defaults = (defaults or {})
         
-    def __str__(self): return 'P2P Search API connected to %s' % self.node
-    def __repr__(self): return 'Search API: %s - defaults:%s' % (self.__get_url(), self.defaults)
-    
+    def __str__(self):
+        return 'P2P Search API connected to %s' % self.node
+    def __repr__(self):
+        return 'Search API: %s - defaults:%s' % (self.__get_url(), self.defaults)
 
     def set_defaults(self, defaults):
         """Set the defaults that will be used every time the node is contacted. For example::
@@ -67,7 +65,7 @@ class P2P(object):
 
         set_defaults({})
 
-."""
+        ."""
         self.defaults = {}
 
     def add_defaults(self, **defaults): 
@@ -85,9 +83,10 @@ This replaces but not removes existing keys."""
 
 This would remove all values for both *institute* and *model* leaving everything else intact.""" 
         for k in def_keys: 
-            if k in self.defaults: del self.defaults[k]
-            elif k + '_not_' in self.defaults: del self.defaults[k + '_not_']
-
+            if k in self.defaults:
+                del self.defaults[k]
+            elif k + '_not_' in self.defaults:
+                del self.defaults[k + '_not_']
 
     def duplicate(self):
         """Create a duplicate of this p2p connection.
@@ -102,23 +101,27 @@ This would remove all values for both *institute* and *model* leaving everything
     def __constraints_to_str(self, constraints, **defaults):
         """Transform the constraints passed into a url string. Apply defaults on top of it."""
         proc_const = []
-        #work on a copy
+        # work on a copy
         constraint_dict = self.defaults.copy()
 
-        #apply the object wide defaults
-        if constraints: constraint_dict.update(constraints)
+        # apply the object wide defaults
+        if constraints:
+            constraint_dict.update(constraints)
 
-        #if a method defines some defaults apply them on top
-        #note this works with positive changes, you can't remove anything (yet)
-        if defaults: constraint_dict.update(defaults)
+        # if a method defines some defaults apply them on top
+        # note this works with positive changes, you can't remove anything (yet)
+        if defaults:
+            constraint_dict.update(defaults)
         
-        #encode constraints into query
+        # encode constraints into query
         if constraint_dict:
             for key, value in constraint_dict.items():
-                #negations might have been coded as <property>_not_ if not passed in a dictionary
-                #just transform them to suffix '!'
-                if key[-5:] == '_not_': key = key[:-5] + '!'
-                if isinstance(value, list): value = ('&%s=' % key).join(map(str, value))
+                # negations might have been coded as <property>_not_ if not passed in a dictionary
+                # just transform them to suffix '!'
+                if key[-5:] == '_not_':
+                    key = key[:-5] + '!'
+                if isinstance(value, list):
+                    value = ('&%s=' % key).join(map(str, value))
                 proc_const.append('%s=%s' % (key, value))
 
         return '&'.join(proc_const)
@@ -146,7 +149,7 @@ This would remove all values for both *institute* and *model* leaving everything
 
 :param type: type of elements to return.
 :type type: :class:`P2P.TYPE`
-:param constraints: dictinary with any P2P Search API constraints.
+:param constraints: dictionary with any P2P Search API constraints.
 :returns: all documents returned from the P2P Search API."""
         query = self.__constraints_to_str(constraints, type=type)
         return self.raw_search(query)['response']
@@ -156,7 +159,7 @@ This would remove all values for both *institute* and *model* leaving everything
         if 'fields' in constraints: 
             del constraints['fields']
         datasets = set([(d['master_id'], int(d['version'])) for d in self.datasets(
-                        fields='master_id,version',batch_size=batch_size,**constraints)])
+                        fields='master_id,version', batch_size=batch_size, **constraints)])
         return datasets
 
     def get_datasets(self, **constraints):
@@ -164,7 +167,7 @@ This would remove all values for both *institute* and *model* leaving everything
 :returns: a list of dataset names. There's no limit imposed in the method itself, So it will
           run out of memory if too many datasets are tried to be retrieved. Use the constraint limit=N to get
           just the first "N" records.
-:param constraints: dictinary with any P2P Search API constraints.
+:param constraints: dictionary with any P2P Search API constraints.
 """
 
         return [d for d in self.datasets(**constraints)]
@@ -174,13 +177,13 @@ This would remove all values for both *institute* and *model* leaving everything
 :param batch_size: defines the size of the retrieved batch. It means a query will 
                    be sent to Solr every ``batch_size`` entries from the result set.
 :type batch_size: int
-:param constraints: dictinary with any P2P Search API constraints."""
-        #if we have a limit use it also here.
+:param constraints: dictionary with any P2P Search API constraints."""
+        # if we have a limit use it also here.
         max_items = sys.maxint
         if 'limit' in constraints:
             max_items = int(constraints['limit'])
         else:
-            #if not set the query limit to the batch size
+            # if not set the query limit to the batch size
             constraints['limit'] = batch_size
         if 'offset' in constraints:
             sofar = int(constraints['offset'])
@@ -190,14 +193,13 @@ This would remove all values for both *institute* and *model* leaving everything
 
         result = self.search(offset=sofar, **constraints)
 
-
         total = min(max_items, result['numFound'])
         retrieved = len(result['docs'])
         for d in result['docs']:
             yield d
         
         while sofar + retrieved < total:
-            result = self.search(offset=sofar+retrieved,**constraints)
+            result = self.search(offset=sofar+retrieved, **constraints)
             total = min(result['numFound'], max_items)
             for d in result['docs']:
                 yield d
@@ -208,21 +210,22 @@ This would remove all values for both *institute* and *model* leaving everything
         """the same as :class:`P2P.datasets`, but sets the type to 'File'. It's just a commodity function to
 improve readability.
 
-:param constraints: dictinary with any P2P Search API constraints."""
+:param constraints: dictionary with any P2P Search API constraints."""
         return self.datasets(type=type, **constraints)
 
     def get_facets(self, *facets, **constraints):
         """
 :param facets: list of facets to query for.
-:param constraints: dictinary with any P2P Search API constraints.
+:param constraints: dictionary with any P2P Search API constraints.
 :returns: ([{facet:{facet_value:count}}]) the facets count from the server."""
         if 'limit' in constraints:
             constraints = constraints.copy()
             del constraints['limit']
 
-        #facets encoded as a list
-        if len(facets)==1 and isinstance(facets[0], list): facets=facets[0]
-        query= '%s&limit=0&facets=%s' % (self.__constraints_to_str(constraints), ','.join(facets))
+        # facets encoded as a list
+        if len(facets) == 1 and isinstance(facets[0], list):
+            facets = facets[0]
+        query = '%s&limit=0&facets=%s' % (self.__constraints_to_str(constraints), ','.join(facets))
 
         response = self.raw_search(query)['facet_counts']['facet_fields']
         result = {}
@@ -230,137 +233,20 @@ improve readability.
             result[facet] = dict(zip(response[facet][0::2], response[facet][1::2]))
         return result
 
-
-
     @staticmethod
     def show(dictio, return_str=False):
         """Pretty print json (or any dict)"""
         json_str = json.dumps(dictio, sort_keys=True, indent=2)
-        if return_str: return json_str
+        if return_str:
+            return json_str
         print json_str
 
     @staticmethod
     def extract_catalog(dictio):
-        """Extract the catalog from this json returned info. Only 1 cataog is expected."""
+        """Extract the catalog from this json returned info. Only 1 catalog is expected."""
         if 'url' in dictio:
-            result = [ e.split('.xml')[0]+'.xml' for e in dictio['url'] if e[-7:] == 'Catalog']
+            result = [e.split('.xml')[0]+'.xml' for e in dictio['url'] if e[-7:] == 'Catalog']
             if result and len(result) == 1:
                 return result[0]
             else:
                 raise Exception("Can't find unique catalog")
-
-
-#**** COMMAND LINE ****
-#===============================================================================
-# def auto_doc(message=''):
-#    import re, os
-#    script_file = sys.argv[0]
-# 
-#    re_start = re.compile('.*\*!!!\*$')
-#    re_end = re.compile('^[ \t]*$')
-#    re_entries= re.compile("^[^']*'([^']*)'[^']*(?:'([^']*)')?[^#]*#(.*)$")
-#    parsing=False
-#    results = []
-#    for line in open(script_file, 'r'):
-#        if parsing:
-#            items = re_entries.match(line)
-#            if items:
-#                flag, flag_opt, mesg = items.groups()
-#                if flag_opt: flag = '%s, %s' % (flag, flag_opt)
-#                results.append('  %-20s : %s' % (flag, mesg))
-#            if re_end.match(line): break
-#        elif re_start.match(line): parsing = True
-# 
-#    if results: print '%s%s [opt]\nopt:\n%s' % (message, os.path.basename(script_file), '\n'.join(results))
-#    else: print '%s %s' % (os.path.basename(script_file), message)
-# 
-# def usage(message=None):
-#    if message: auto_doc(message)
-#    else: auto_doc()
-# 
-# 
-# def main(argv=None):
-#    import getopt
-#    import re
-#    facet_pat = re.compile(r'(.*[^\\])=(.*)')
-# 
-#    if argv is None: argv = sys.argv[1:]
-#    try:
-#        args, lastargs = getopt.getopt(argv, "f:hd", ['help', 'debug', 'facet=','query=', 'list-datasets', 'show-facet='])
-#    except getopt.error:
-#        print sys.exc_info()[:3]
-#        return 1
-# 
-#    DEBUG = False
-#    datasets = False
-#    facets = {}
-#    query=None
-#    show_facets = []
-#    #parse arguments *!!!*
-#    for flag, arg in args:
-#        if flag=='-f' or flag=='--facet':   #       Set facet for search (e.g. institute=MPI) can be used multiple times.
-#            facet, value = facet_pat.match(arg).groups()
-#            if facet in facets:
-#                #accept same value multiple times!
-#                if not isinstance(facets[facet], list): facets[facet] = [facets[facet]]
-#                facets[facet].append(value)
-#            else: 
-#                facets[facet] = value
-#        elif flag=='--show-facet':         #<list> :List all values for the given facet (might be defined multiple times)
-#            show_facets.append(arg)
-#        elif flag=='--list-datasets':       #       List datasets found using the p2p interface
-#            datasets = True
-#        elif flag=='--query':               #<list> :Display results from <list> queried fields
-#            query=arg
-#        elif flag == '-d' or flag == '--debug': #turn on debuging info
-#            DEBUG = True
-#        elif flag=='-h' or flag=='--help':        #This help
-#            usage('Interact with p2p index nodes via the search API\n')
-#            return 0
-# 
-# 
-#    #if empty leave it alone    
-#    #handle constraints as the last items in the argument list
-#    for arg in lastargs:
-#        if '=' not in arg:
-#            raise Exception("Invalid format for query: %s" % arg)
-#        
-#        items = arg.split('=')
-#        if len(items==1):
-#            facets[items[0]] = True
-#        else:
-#            facets[items[0]] = '='.join(items[1:])
-#    
-#    p2p = P2P()
-#        
-#    if DEBUG:
-#        sys.stderr.write("Searching dict: %s\n" % p2p.show(facets, return_str=True))
-# 
-#    
-# 
-#    if datasets: print '\n'.join(['%s#%s' % d for d in sorted(p2p.get_datasets_names(**facets))])
-#    if query: 
-#        if len(query.split(',')) > 1:
-#            #we get multiple fields queried, return in a tructured fashion
-#            p2p.show(p2p.get_datasets(fields=query,**facets))
-#        else:
-#            #if only one then return directly
-#            print '\n'.join([str(d[query]) for d in p2p.get_datasets(fields=query,**facets)])
-#        
-#    if show_facets:
-#        results = p2p.get_facets(show_facets, **facets)
-#        #render them
-#        for facet_key in sorted(results):
-#            print '[%s]\n\t%s' % (facet_key, '\n\t'.join([ '%s: %s' % (k,results[facet_key][k])  for k in sorted(results[facet_key])]))
-#    return 0
-#            
-# 
-# 
-# if __name__ == '__main__':
-#    result=main(None)
-#    if isinstance(result, int):
-#        if result != 0: usage()
-#        sys.exit(result)
-#===============================================================================
-
-
