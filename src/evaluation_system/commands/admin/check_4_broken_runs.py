b62819653cb07ca49a5f8d808822bf8c85ff1be0
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2016-08-11 18:13:54 +0200

    added FAIL, TIMEOUT, COMPLETED to status check

--- src/evaluation_system/commands/admin/check_4_broken_runs.py
+++ src/evaluation_system/commands/admin/check_4_broken_runs.py
@@ -24,14 +24,19 @@ class Command(FrevaBaseCommand):
 
     def _run(self):
         # get all jobs with status "running" and started with scheduler
+        import datetime
+        
         running_jobs = History.objects.filter(status=History.processStatus.running).exclude(slurm_output=0)
         for job in running_jobs:
             slurm_status = job.get_slurm_status()
-            if 'cancelled' in slurm_status.lower():
+            if 'cancelled' in slurm_status.lower() or 'timeout' in slurm_status.lower() or 'fail' in slurm_status.lower():
+                print slurm_status, job.tool
                 print 'Setting job %s to broken' % job.id
                 job.status = job.processStatus.broken
                 job.save()
-
+            elif 'completed' in slurm_status.lower():
+                job.status = job.processStatus.finished
+                job.save()
 
 if __name__ == "__main__":  # pragma: nocover
     Command().run()
