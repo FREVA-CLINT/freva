dae8b01151dcf131904573d2c8c34fc2b21f5967
k204230 <bergemann@dkrz.de>
2021-02-10 07:43:22 +0100

    Add method to compare strings

--- src/evaluation_system/tests/__init__.py
+++ src/evaluation_system/tests/__init__.py
@@ -1,3 +1,22 @@
-import test_setup
-import os
-os.environ['EVALUATION_SYSTEM_CONFIG_FILE'] = os.path.dirname(__file__) + '/test.conf'
+import sys
+from difflib import SequenceMatcher
+
+
+def run_command_with_capture(cmd, stdout, args_list=[], stderr=None):
+    sys.stdout = stdout
+    stdout.startCapturing()
+    stdout.reset()
+    cmd.run(args_list)
+    stdout.stopCapturing()
+    res = stdout.getvalue()
+    return res
+
+def similar_string(a, b, thresh=0.98):
+    a = a.strip().replace('\n', ' ').replace('\t', ' ').replace('  ', ' ').strip()
+    b = b.strip().replace('\n', ' ').replace('\t', ' ').replace('  ', ' ' ).strip()
+    #print('a', a.split(' '))
+    #print('\n')
+    #print('b', b.split(' '))
+    ratio = SequenceMatcher(None, a, b).ratio()
+    #print(ratio)
+    return ratio >= thresh
