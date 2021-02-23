16801eb9a425767e4e1f5f1301751b6c93ffc41b
k204230 <bergemann@dkrz.de>
2021-02-23 11:46:24 +0100

    Make run_command_with_capture call more generic

--- src/evaluation_system/tests/__init__.py
+++ src/evaluation_system/tests/__init__.py
@@ -2,11 +2,11 @@ import sys
 from difflib import SequenceMatcher
 
 
-def run_command_with_capture(cmd, stdout, args_list=[], stderr=None):
+def run_command_with_capture(cmd, stdout, args_list=[], stderr=None, attr='run'):
     sys.stdout = stdout
     stdout.startCapturing()
     stdout.reset()
-    cmd.run(args_list)
+    getattr(cmd, attr)(args_list)
     stdout.stopCapturing()
     res = stdout.getvalue()
     return res
@@ -14,9 +14,9 @@ def run_command_with_capture(cmd, stdout, args_list=[], stderr=None):
 def similar_string(a, b, thresh=0.98):
     a = a.strip().replace('\n', ' ').replace('\t', ' ').replace('  ', ' ').strip()
     b = b.strip().replace('\n', ' ').replace('\t', ' ').replace('  ', ' ' ).strip()
-    #print('a', a.split(' '))
-    #print('\n')
-    #print('b', b.split(' '))
+    print('a', a.split(' '))
+    print('\n')
+    print('b', b.split(' '))
     ratio = SequenceMatcher(None, a, b).ratio()
-    #print(ratio)
+    print(ratio)
     return ratio >= thresh
