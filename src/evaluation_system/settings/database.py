d290ad28210d735afca0d8a930a8828f28c05a3e
k204230 <bergemann@dkrz.de>
2021-05-06 20:26:59 +0200

    catch error only for settings.configure call

--- src/evaluation_system/settings/database.py
+++ src/evaluation_system/settings/database.py
@@ -1,30 +1,31 @@
 from django.conf import settings
 from evaluation_system.misc import config
 
-import json,time
+import json, time
 import django
 
 SETTINGS = dict()
 
-try:
-    # Application definition
-    SETTINGS['INSTALLED_APPS'] = (
-        'django.contrib.flatpages',
-	'django.contrib.auth',  # We need this to access user groups
-        'django.contrib.sites'
-    )
-    SETTINGS['DATABASES'] = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': config.get(config.DB_DB),
-            'USER': config.get(config.DB_USER),
-            'PASSWORD': config.get(config.DB_PASSWD),
-            'HOST': config.get(config.DB_HOST),   # Or an IP Address that your DB is hosted on
-            'PORT': '3306',
+# Application definition
+SETTINGS['INSTALLED_APPS'] = (
+    'django.contrib.auth',  # We need this to access user groups
+    'django.contrib.flatpages',
+    'django.contrib.contenttypes',
+    'django.contrib.sites'
+)
+SETTINGS['DATABASES'] = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config.get(config.DB_DB),
+        'USER': config.get(config.DB_USER),
+        'PASSWORD': config.get(config.DB_PASSWD),
+        'HOST': config.get(config.DB_HOST),   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
 
-        }
     }
+}
+try:
     settings.configure(**SETTINGS)
-    django.setup()
-except:
+except RuntimeError:
     pass
+django.setup()
