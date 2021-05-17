b2be960081ca2aaf13c7137c2c60dc07f2440c7c
k204230 <bergemann@dkrz.de>
2021-05-17 17:44:07 +0200

    Rename entry point module

--- setup.py
+++ setup.py
@@ -5,23 +5,8 @@ import re
 from setuptools import setup, find_packages
 import sys
 
+from install import find_version, read
 
-def get_script_path():
-    return osp.dirname(osp.realpath(sys.argv[0]))
-
-
-def read(*parts):
-    return open(osp.join(get_script_path(), *parts)).read()
-
-def find_files(path, glob_pattern='*'):
-    return [str(f) for f in Path(path).rglob(glob_pattern)]
-
-def find_version(*parts):
-    vers_file = read(*parts)
-    match = re.search(r'^__version__ = "(\d+.\d+)"', vers_file, re.M)
-    if match is not None:
-        return match.group(1)
-    raise RuntimeError("Unable to find version string.")
 
 setup(name="evaluation_system",
       version=find_version("src/evaluation_system", "__init__.py"),
@@ -32,6 +17,7 @@ setup(name="evaluation_system",
       long_description_content_type='text/markdown',
       license="BSD-3-Clause",
       packages=find_packages('src'),
+      package_dir={"":"src"},
       extras_require={
         'docs': [
               'sphinx',
@@ -49,7 +35,7 @@ setup(name="evaluation_system",
               'testpath',
           ]
         },
-      entry_points={'console_scripts': ['freva = evaluation_system.main:_run']},
+      entry_points={'console_scripts': ['freva = evaluation_system.commands._main:_run']},
       python_requires='>=3.7',
       classifiers=[
           'Development Status :: 3 - Alpha',
