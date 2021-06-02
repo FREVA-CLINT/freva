c22ac155e63e209a6f8ae27319e263a41a649205
k204230 <bergemann@dkrz.de>
2021-06-02 07:21:09 +0200

    Add verbose option

--- .docker/loadfreva.sh
+++ .docker/loadfreva.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
 # Run initdb, then start services in the foreground
-set -xe
-sudo service mysql start
+set -e
+if [ "$VERBOSE" == "yes" ];then
+    set -xe
+fi
+
+exec sudo service mysql start
 exec /opt/docker-solr/scripts/solr-foreground &
 wait
 
