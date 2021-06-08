24ced85866145e74147c8cd86e7c39d6bdcf577e
k204230 <bergemann@dkrz.de>
2021-06-08 22:54:51 +0200

    Set completion style

--- etc/autocomplete.bash
+++ etc/autocomplete.bash
@@ -11,6 +11,13 @@ if [ "$sh" = "bash" ] || [ "$sh" = "sh" ];then
 elif [ "$sh" = "zsh" ];then
     etc_dir=$(dirname $(readlink -f $0))
     let zsh_major=${ZSH_VERSION:0:1}
+    setopt COMPLETE_ALIASES
+    zstyle ':completion::complete:*' gain-privileges 1
+    autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+    zle -N up-line-or-beginning-search
+    zle -N down-line-or-beginning-search
+    [[ -n "${key[Up]}"   ]] && bindkey -- "${key[Up]}"   up-line-or-beginning-search
+    [[ -n "${key[Down]}" ]] && bindkey -- "${key[Down]}" down-line-or-beginning-search
     fpath+=($etc_dir/zsh)
     source /usr/share/zsh/functions/Completion/compinit 1> /dev/null
 fi
