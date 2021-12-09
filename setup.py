3e39d1607e0e3fe26004e89fce11745d0bd8064f
k204230 <bergemann@dkrz.de>
2021-12-09 17:07:55 +0100

    Add new entry point scripts

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-import os
-import os.path as osp
-from pathlib import Path
-import re
 from setuptools import setup, find_packages
-import sys
 
 from deploy import find_version, read
 
-
+COMMANDS = ["databrowser", "esgf", "history", "plugin", "crawl-my-data"]
 
 setup(name="evaluation_system",
       version=find_version("src/evaluation_system", "__init__.py"),
@@ -19,40 +14,44 @@ setup(name="evaluation_system",
       long_description_content_type='text/markdown',
       license="BSD-3-Clause",
       packages=find_packages('src'),
-      package_dir={"":"src"},
-      install_requires=['Django',
-                        'numpy',
-                        'python-git',
-                        'mysqlclient',
-                        'pymysql',
-                        'pandas',
-                        'configparser',
-                        'Pillow',
-                        'PyPDF2',],
+      package_dir={"": "src"},
+      install_requires=["Django",
+                        "humanize",
+                        "mysqlclient",
+                        "numpy",
+                        "python-git",
+                        "pymysql",
+                        "pandas",
+                        "Pillow",
+                        "PyPDF2"],
       extras_require={
-        'docs': [
-              'sphinx',
-              'nbsphinx',
-              'recommonmark',
-              'ipython',  # For nbsphinx syntax highlighting
-              'sphinxcontrib_github_alt',
+        "docs": [
+              "sphinx",
+              "nbsphinx",
+              "recommonmark",
+              "ipython",  # For nbsphinx syntax highlighting
+              "sphinxcontrib_github_alt",
               ],
-        'test': [
-              'pytest',
-              'nbformat',
-              'pytest-html',
-              'pytest-env',
-              'pytest-cov',
-              'nbval',
-              'h5netcdf',
-              'mock',
-              'requests_mock',
-              'allure-pytest',
-              'python-swiftclient',
-              'testpath',
+        "test": [
+              "pytest",
+              "nbformat",
+              "pytest-html",
+              "pytest-env",
+              "pytest-cov",
+              "nbval",
+              "h5netcdf",
+              "mock",
+              "requests_mock",
+              "allure-pytest",
+              "python-swiftclient",
+              "testpath",
           ]
         },
-      entry_points={'console_scripts': ['freva = evaluation_system.commands._main:_run']},
+      entry_points={"console_scripts": [
+          "freva = freva.cli:main"]+[
+              f"freva-{cmd} = freva.cli.{cmd.replace('-', '_')}:main" for cmd in COMMANDS
+              ]
+      },
       python_requires='>=3.8',
       classifiers=[
           'Development Status :: 3 - Alpha',
@@ -62,9 +61,9 @@ setup(name="evaluation_system",
           'License :: OSI Approved :: BSD License',
           'Operating System :: POSIX :: Linux',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
           'Topic :: Scientific/Engineering :: Data Analysis',
           'Topic :: Scientific/Engineering :: Earth Sciences',
       ]
