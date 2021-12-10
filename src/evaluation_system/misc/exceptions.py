95a6840f9e7ed573b713cf7a66da9c5f0c87e541
k204230 <bergemann@dkrz.de>
2021-12-10 18:23:13 +0100

    Add commanderror and context manager for suppressing stacktraces

--- src/evaluation_system/misc/exceptions.py
+++ src/evaluation_system/misc/exceptions.py
@@ -1,5 +1,7 @@
 """Definitions of custom exections."""
 
+from contextlib import contextmanager
+import sys
 
 class PluginNotFoundError(Exception):
     """Exeption Definition for missing Plugins."""
@@ -13,6 +15,26 @@ class ParameterNotFoundError(Exception):
     """Thrown if some parameter is not in the database."""
     pass
 
+class CommandError(Exception):
+    """
+    Generic exception to raise and log different fatal errors.
+    """
+    def __init__(self, msg):
+        super(CommandError).__init__(type(self))
+        self.msg = f"{msg}\nUse --help for getting help"
+
+    def __str__(self):
+        return self.msg
+
+@contextmanager
+def hide_exception():
+    """
+    Suppress traceback, only print the exception type and value
+    """
+    default_value = getattr(sys, "tracebacklimit", 1000)
+    sys.tracebacklimit = 0
+    yield
+    sys.tracebacklimit = default_value  # revert changes
 
 
 
