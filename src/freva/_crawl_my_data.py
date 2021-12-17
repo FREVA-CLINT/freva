12957c5d1534c9f58fae69466f5efe4f88c7c8f8
k204230 <bergemann@dkrz.de>
2021-12-17 12:32:34 +0100

    Add the crawl_command

--- src/freva/_crawl_my_data.py
+++ src/freva/_crawl_my_data.py
@@ -7,12 +7,12 @@ from typing import Optional, Union, Tuple
 from evaluation_system.model.user import User
 from evaluation_system.misc import config
 from evaluation_system.misc.exceptions import ValidationError, ConfigurationException
+from evaluation_system.model.solr_core import SolrCore
 
 __all__ = ["crawl_my_data"]
 
 
 def _validate_user_dirs(*crawl_dirs: Optional[Union[str, Path]]) -> Tuple[Path]:
-    project = config.get("project_name")
     try:
         root_path = Path(config.get("project_data")).absolute()
     except ConfigurationException:
@@ -20,9 +20,7 @@ def _validate_user_dirs(*crawl_dirs: Optional[Union[str, Path]]) -> Tuple[Path]:
         root_path = Path(config.get("project_data")).absolute()
     user_root_path = root_path / f"user-{User().getName()}"
     user_paths = ()
-    if not isinstance(crawl_dirs, tuple):
-        crawl_dirs = (crawl_dirs,)
-    for crawl_dir in crawl_dirs:
+    for crawl_dir in crawl_dirs or (user_root_path,):
         crawl_dir = Path(crawl_dir or user_root_path).expanduser().absolute()
         if not crawl_dir.is_relative_to(root_path):
             raise ValidationError(f"You are only allowed to crawl data in {root_path}")
@@ -47,5 +45,8 @@ def crawl_my_data(*crawl_dirs: Optional[Union[str, Path]], dtype: str = "fs") ->
         raise NotImplementedError('Only data on POSIX file system is supported')
     print(f"Status: crawling ...", end="")
     for crawl_dir in _validate_user_dirs(*crawl_dirs):
-        continue
+        SolrCore.load_fs(crawl_dir,
+                         chunk_size=200,
+                         abort_on_errors=True,
+                         drs_type='crawl_my_data')
     print("ok", flush=True)
