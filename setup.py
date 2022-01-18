ccea5d48f1d2342cf95f776d8d297b1f298df376
antarcticrainforest <martin.bergemann@posteo.org>
2022-01-18 11:10:33 +0100

    Add datafiles for completions

--- setup.py
+++ setup.py
@@ -1,70 +1,132 @@
 #!/usr/bin/env python
+from pathlib import Path
 from setuptools import setup, find_packages
-
+import sys
+from typing import List, Tuple
+from tempfile import TemporaryDirectory
 from deploy import find_version, read
 
 COMMANDS = ["databrowser", "esgf", "history", "plugin", "crawl-my-data"]
+this_dir = Path(__file__).parent
+COMPLETION_DIR = this_dir / "assets" / "completions"
 
-setup(name="evaluation_system",
-      version=find_version("src/evaluation_system", "__init__.py"),
-      author="German Climate Computing Centre (DKRZ)",
-      maintainer="Climate Informatics and Technology (CLINT)",
-      description="Free Evaluation and Analysis Framework (Freva) ",
-      long_description=read("README.md"),
-      long_description_content_type='text/markdown',
-      license="BSD-3-Clause",
-      packages=find_packages('src'),
-      package_dir={"": "src"},
-      install_requires=["Django",
-                        "humanize",
-                        "mysqlclient",
-                        "numpy",
-                        "python-git",
-                        "pymysql",
-                        "pandas",
-                        "Pillow",
-                        "PyPDF2"],
-      extras_require={
-        "docs": [
-              "sphinx",
-              "nbsphinx",
-              "recommonmark",
-              "ipython",  # For nbsphinx syntax highlighting
-              "sphinxcontrib_github_alt",
-              ],
-        "test": [
-              "pytest",
-              "nbformat",
-              "pytest-html",
-              "pytest-env",
-              "pytest-cov",
-              "nbval",
-              "h5netcdf",
-              "mock",
-              "requests_mock",
-              "allure-pytest",
-              "python-swiftclient",
-              "testpath",
-          ]
+
+def prep_tcsh_completion(tempdir: Path) -> List[Tuple[str, List[str]]]:
+    """Create completion scripts for tcsh."""
+
+    script_dir = COMPLETION_DIR.relative_to(this_dir) / "tcsh"
+    completion_helper = (
+        Path(sys.prefix)
+        / "share"
+        / "tcsh-completion"
+        / "helpers"
+        / "tcsh-completion.bash"
+    )
+    script_paths = [
+        (
+            str(Path("share") / "tcsh-completion" / "helpers"),
+            [str(script_dir / f"{completion_helper.name}")],
+        ),
+        (
+            str(Path("share") / "tcsh-completion" / "completion"),
+            [str(tempdir.relative_to(this_dir) / "freva")],
+        ),
+    ]
+    completion_script = [
+        "set autoexpand",
+        f"complete freva 'p,*,`bash {completion_helper} \"${{COMMAND_LINE}}\"`,'",
+    ]
+    for cmd in COMMANDS:
+        completion_script.append(
+            f"complete {cmd} 'p,*,`bash {completion_helper} \"${{COMMAND_LINE}}\"`,'"
+        )
+    with (tempdir / "freva").open("w") as f:
+        f.write("\n".join(completion_script))
+    return script_paths
+
+
+def gather_completion_scripts(tempdir: Path) -> List[Tuple[str, List[str]]]:
+    """Gather all data_files related to shell completion scripts."""
+
+    shells = {
+        "zsh": Path("share") / "freva" / "zsh" / "completions",
+        "bash": Path("share") / "bash-completion" / "completion",
+        "fish": Path("share") / "fish" / "completions",
+    }
+    data_files = []
+    for shell, target_path in shells.items():
+        comp_files = [
+            str(f.relative_to(this_dir)) for f in (COMPLETION_DIR / shell).rglob("*")
+        ]
+        data_files.append((str(target_path), comp_files))
+    return data_files + prep_tcsh_completion(tempdir)
+
+
+with TemporaryDirectory(dir=COMPLETION_DIR) as td:
+    data_files = gather_completion_scripts(Path(td))
+    entry_points = ["freva = freva.cli:main"]
+    for cmd in COMMANDS:
+        entry_points.append(f"freva-{cmd} = freva.cli.{cmd.replace('-', '_')}:main")
+    setup(
+        name="evaluation_system",
+        version=find_version("src/evaluation_system", "__init__.py"),
+        author="German Climate Computing Centre (DKRZ)",
+        maintainer="Climate Informatics and Technology (CLINT)",
+        description="Free Evaluation and Analysis Framework (Freva) ",
+        long_description=read("README.md"),
+        long_description_content_type="text/markdown",
+        license="BSD-3-Clause",
+        packages=find_packages("src"),
+        package_dir={"": "src"},
+        data_files=data_files,
+        install_requires=[
+            "Django",
+            "humanize",
+            "mysqlclient",
+            "numpy",
+            "python-git",
+            "pymysql",
+            "pandas",
+            "Pillow",
+            "PyPDF2",
+        ],
+        extras_require={
+            "docs": [
+                "sphinx",
+                "nbsphinx",
+                "recommonmark",
+                "ipython",  # For nbsphinx syntax highlighting
+                "sphinxcontrib_github_alt",
+            ],
+            "test": [
+                "pytest",
+                "nbformat",
+                "pytest-html",
+                "pytest-env",
+                "pytest-cov",
+                "nbval",
+                "h5netcdf",
+                "mock",
+                "requests_mock",
+                "allure-pytest",
+                "python-swiftclient",
+                "testpath",
+            ],
         },
-      entry_points={"console_scripts": [
-          "freva = freva.cli:main"]+[
-              f"freva-{cmd} = freva.cli.{cmd.replace('-', '_')}:main" for cmd in COMMANDS
-              ]
-      },
-      python_requires='>=3.8',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Topic :: Scientific/Engineering :: Data Analysis',
-          'Topic :: Scientific/Engineering :: Earth Sciences',
-      ]
-      )
+        entry_points={"console_scripts": entry_points},
+        python_requires=">=3.8",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Topic :: Scientific/Engineering :: Data Analysis",
+            "Topic :: Scientific/Engineering :: Earth Sciences",
+        ],
+    )
