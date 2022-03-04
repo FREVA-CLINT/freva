e046d1404923a6e1eda7fa34519bcf1b4ad8e8d6
k204230 <bergemann@dkrz.de>
2022-03-04 13:38:22 +0100

    Set log level to ERROR for cmd completion

--- src/freva/cli/__main__.py
+++ src/freva/cli/__main__.py
@@ -1,8 +1,9 @@
 """Module to print command argument line argument choices."""
 
 import sys
-from .utils import print_choices
-
+import logging
+from .utils import print_choices, logger
+logger.setLevel(logging.ERROR)
 
 try:
     print_choices(sys.argv[1:])
