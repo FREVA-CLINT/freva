53ee0ca42807ecdfdad6f4fdf19ab7d1841a90ce
k204230 <bergemann@dkrz.de>
2022-05-28 09:02:02 +0200

    Update resources to work with binder

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -3,10 +3,11 @@ set -e
 if [ "$VERBOSE" == "yes" ];then
     set -xe
 fi
-/usr/bin/git config --global init.defaultBranch main &> /dev/null
-/usr/bin/git config --global user.email "user@docker.org" &> /dev/null
-/usr/bin/git config --global user.name "Freva" &> /dev/null
 export PATH=/opt/evaluation_system/bin:$PATH
 mysqld_safe &> ${MYSQL_LOGS_DIR}/mysqld-3306-console.log &
 /opt/solr/bin/solr start -s ${SOLR_HOME} -v
+if [ "${IS_BINDER}" = "true" ];then
+    cd ~/.evaluation_system && make dummy-data && cd ~
+    rm -r ~/.evaluation_system
+fi
 exec "$@"
