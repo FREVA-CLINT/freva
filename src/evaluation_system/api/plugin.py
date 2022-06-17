7beea0d87144750c27d29d87d78ee2da74c2dc86
k204230 <bergemann@dkrz.de>
2022-06-18 00:01:29 +0200

    Fix mypy issues

--- src/evaluation_system/api/plugin.py
+++ src/evaluation_system/api/plugin.py
@@ -479,7 +479,7 @@ class PluginAbstract(abc.ABC):
         ensemble: str = "r0i0p0",
         time_frequency: Optional[str] = None,
         variable: Optional[str] = None,
-    ) -> None:
+    ) -> Path:
         """Add Plugin output data to the solr database.
 
         This methods crawls the plugin output data directory and adds
@@ -523,6 +523,10 @@ class PluginAbstract(abc.ABC):
         variable: str, default: None
             Default variable facet. If None is given (default) the variable
             will be retrieved from the output files.
+
+        Returns
+        -------
+            Path: Path to the new directory that containes the data.
         """
         _, plugin_version = repository.get_version(self.class_basedir)
         plugin_version = plugin_version or "no_plugin_version"
