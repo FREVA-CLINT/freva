3cb50455925040f187b227ed44a7a2c3ef36b1b3
antarcticrainforest <martin.bergemann@posteo.org>
2022-10-10 15:49:19 +0200

    Reformat

--- src/evaluation_system/api/user_data.py
+++ src/evaluation_system/api/user_data.py
@@ -42,9 +42,7 @@ class DataReader:
 
         self.paths = paths
         self.defaults = defaults
-        drs_config: dict[str, Any] = config.get_drs_config()[
-            self.drs_specification
-        ]
+        drs_config: dict[str, Any] = config.get_drs_config()[self.drs_specification]
         self.root_dir = Path(drs_config["root_dir"]).expanduser().absolute()
         self.parts_dir: list[str] = [
             d for d in drs_config["parts_dir"] if d != "file_name"
@@ -176,14 +174,10 @@ class DataReader:
                 continue
             variables.append(var)
         if len(variables) != 1:
-            raise ValueError(
-                f"Only one data variable allowed found: {variables}"
-            )
+            raise ValueError(f"Only one data variable allowed found: {variables}")
         _data = self.defaults.copy()
         _data.setdefault("variable", variables[0])
-        _data.setdefault(
-            "time_frequency", self.get_time_frequency(dt, time_freq)
-        )
+        _data.setdefault("time_frequency", self.get_time_frequency(dt, time_freq))
         _data["time"] = time_str
         _data.setdefault("cmor_table", _data["time_frequency"])
         _data.setdefault("version", "")
@@ -208,9 +202,7 @@ class DataReader:
             new_dirs[v_index] = new_version
         return new_dirs
 
-    def file_name_from_metdata(
-        self, path: os.PathLike, override: bool = False
-    ) -> Path:
+    def file_name_from_metdata(self, path: os.PathLike, override: bool = False) -> Path:
         """Construct file name matching the DRS Spec. from given input path.
 
         Parameters
@@ -243,9 +235,7 @@ class DataReader:
                 f"Please add the following key manually: {error}"
             ) from error
         if not meta_data["version"] and "version" in self.parts_dir:
-            dir_parts = self._create_versioned_path(
-                dir_parts, override=override
-            )
+            dir_parts = self._create_versioned_path(dir_parts, override=override)
         dir_path = self.root_dir.joinpath(*dir_parts)
         file_path = self.file_sep.join(file_parts)
         out_dir = (dir_path / file_path).with_suffix(path.suffix)
