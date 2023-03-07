d3d10860c2a2b5619fc69954b3f8efbd9598a6ae
antarcticrainforest <martin.bergemann@posteo.org>
2023-03-07 12:34:16 +0100

    Do not add output that is of type data

--- src/evaluation_system/api/plugin.py
+++ src/evaluation_system/api/plugin.py
@@ -301,7 +301,9 @@ class PluginAbstract(abc.ABC):
         """Optional tags, that are the plugin can be described with."""
         return [""]
 
-    def run_tool(self, config_dict: Optional[ConfigDictType] = None) -> Optional[Any]:
+    def run_tool(
+        self, config_dict: Optional[ConfigDictType] = None
+    ) -> Optional[Any]:
         """Method executing the tool.
 
         The method should be overidden by the custom plugin tool method.
@@ -350,7 +352,9 @@ class PluginAbstract(abc.ABC):
                 self._user.getUserSchedulerOutputDir(),
                 plugin_name.lower(),
             )
-            self._plugin_out = Path(log_directory) / f"{plugin_name}-{pid}.local"
+            self._plugin_out = (
+                Path(log_directory) / f"{plugin_name}-{pid}.local"
+            )
         return self._plugin_out
 
     def _set_interactive_job_as_running(self, rowid: Optional[int]):
@@ -451,7 +455,10 @@ class PluginAbstract(abc.ABC):
         config_dict = config_dict or {}
         for key, param in self.__parameters__.items():
             tmp_param = self.__parameters__.get_parameter(key)
-            if isinstance(tmp_param, (Directory, CacheDirectory)) and unique_output:
+            if (
+                isinstance(tmp_param, (Directory, CacheDirectory))
+                and unique_output
+            ):
                 if key in config_dict.keys() and config_dict[key] is not None:
                     config_dict[key] = os.path.join(
                         str(config_dict[key]), str(self.rowid)
@@ -464,7 +471,9 @@ class PluginAbstract(abc.ABC):
         :meta private:
         """
         PID = os.getpid()
-        self.call(f'setsid nohup bash -c "kill -9 -- -{PID}"  </dev/null &>/dev/null &')
+        self.call(
+            f'setsid nohup bash -c "kill -9 -- -{PID}"  </dev/null &>/dev/null &'
+        )
         raise SystemExit
 
     @deprecated_method("PluginAbstract", "add_output_to_databrowser")
@@ -548,7 +557,9 @@ class PluginAbstract(abc.ABC):
         project_name = config.get("project_name", "").replace("_", "-")
 
         product_dir = f"{project}.{product}"
-        root_dir = DataReader.get_output_directory() / f"user-{self._user.getName()}"
+        root_dir = (
+            DataReader.get_output_directory() / f"user-{self._user.getName()}"
+        )
         drs_config = dict(
             project=root_dir.name,
             product=product_dir,
@@ -568,7 +579,9 @@ class PluginAbstract(abc.ABC):
             new_file = user_data.file_name_from_metdata(output_file)
             new_file.parent.mkdir(exist_ok=True, parents=True, mode=0o2775)
             shutil.copy(str(output_file), str(new_file))
-        SolrCore.load_fs(root_dir / product_dir, drs_type=user_data.drs_specification)
+        SolrCore.load_fs(
+            root_dir / product_dir, drs_type=user_data.drs_specification
+        )
         return root_dir / product_dir
 
     @deprecated_method("PluginAbstract", "prepare_output")
@@ -620,7 +633,8 @@ class PluginAbstract(abc.ABC):
                 metadata = output_files[file_path]
             if os.path.isfile(file_path):
                 self._extend_output_metadata(file_path, metadata)
-                result[os.path.abspath(file_path)] = metadata
+                if metadata.get("type", "data") != "data":
+                    result[os.path.abspath(file_path)] = metadata
             elif os.path.isdir(file_path):
                 # ok, we got a directory, so parse the contents recursively
                 for file_path in [
@@ -634,10 +648,11 @@ class PluginAbstract(abc.ABC):
                     # update meta data with user entries
                     usermetadata = result.get(os.path.abspath(file_path), {})
                     filemetadata.update(usermetadata)
-
-                    result[os.path.abspath(file_path)] = filemetadata
+                    if filemetadata.get("type", "data") != "data":
+                        result[os.path.abspath(file_path)] = filemetadata
             else:
-                result[os.path.abspath(file_path)] = metadata
+                if filemetadata.get("type", "data") != "data":
+                    result[os.path.abspath(file_path)] = metadata
         return result
 
     def _extend_output_metadata(self, file_path, metadata):
@@ -721,7 +736,9 @@ class PluginAbstract(abc.ABC):
             self.__parameters__.get_help(),
         )
 
-    def get_current_config(self, config_dict: Optional[ConfigDictType] = None) -> str:
+    def get_current_config(
+        self, config_dict: Optional[ConfigDictType] = None
+    ) -> str:
         """Retreive the plugin configuration as string representation.
 
         Parameters
@@ -795,7 +812,9 @@ class PluginAbstract(abc.ABC):
     def class_basedir(self) -> str:
         """Get absolute path to the module defining the plugin class."""
         return os.path.join(
-            *self._split_path(self.wrapper_file)[: -len(self.__module__.split("."))]
+            *self._split_path(self.wrapper_file)[
+                : -len(self.__module__.split("."))
+            ]
         )
 
     @property
@@ -853,7 +872,9 @@ class PluginAbstract(abc.ABC):
             return str_value
 
         else:
-            return self.__parameters__.get_parameter(param_name).parse(str_value)
+            return self.__parameters__.get_parameter(param_name).parse(
+                str_value
+            )
 
     @deprecated_method("PluginAbstract", "setup_configuration")
     def setupConfiguration(
@@ -921,7 +942,9 @@ class PluginAbstract(abc.ABC):
 
         return results
 
-    def read_from_config_parser(self, config_parser: ConfigParser) -> dict[str, str]:
+    def read_from_config_parser(
+        self, config_parser: ConfigParser
+    ) -> dict[str, str]:
         """Reads a configuration from a config parser object.
 
         The values are assumed to be in a section named just like the
@@ -956,7 +979,9 @@ class PluginAbstract(abc.ABC):
         return result
 
     @deprecated_method("PluginAbstract", "read_configuration")
-    def readConfiguration(self, **kwargs) -> dict[str, str]:  # pragma: no cover
+    def readConfiguration(
+        self, **kwargs
+    ) -> dict[str, str]:  # pragma: no cover
         """Deprecated version of the :class:`read_configuration` method.
 
         :meta private:
@@ -1012,7 +1037,9 @@ class PluginAbstract(abc.ABC):
         # store the section header
         if config_dict is None:
             # a default incomplete one
-            config_dict = self.setup_configuration(check_cfg=False, substitute=False)
+            config_dict = self.setup_configuration(
+                check_cfg=False, substitute=False
+            )
         fp.write("[%s]\n" % self.__class__.__name__)
         wrapper = textwrap.TextWrapper(
             width=80,
@@ -1031,7 +1058,9 @@ class PluginAbstract(abc.ABC):
                     help_lines = param.help.splitlines()
                     if param.mandatory:
                         help_lines[0] = "[mandatory] " + help_lines[0]
-                    fp.write("\n".join([wrapper.fill(line) for line in help_lines]))
+                    fp.write(
+                        "\n".join([wrapper.fill(line) for line in help_lines])
+                    )
                     fp.write("\n")
                 value = config_dict.get(param_name, None)
                 if value is None:
@@ -1052,7 +1081,9 @@ class PluginAbstract(abc.ABC):
                     help_lines = key_help.splitlines()
                     if param.mandatory:
                         help_lines[0] = "[mandatory] " + help_lines[0]
-                    fp.write("\n".join([wrapper.fill(line) for line in help_lines]))
+                    fp.write(
+                        "\n".join([wrapper.fill(line) for line in help_lines])
+                    )
                     fp.write("\n")
                 if value is None:
                     # means this is not setup
