78dcf435f2134b99858c9a171310369706a86908
antarcticrainforest <martin.bergemann@posteo.org>
2023-03-20 20:38:57 +0100

    Split up databrowser method

--- src/freva/__init__.py
+++ src/freva/__init__.py
@@ -1,6 +1,6 @@
 from evaluation_system import __version__
 from ._user_data import UserData
-from ._databrowser import databrowser
+from ._databrowser import databrowser, count_values, facet_search
 from ._plugin import (
     run_plugin,
     list_plugins,
@@ -10,10 +10,16 @@ from ._plugin import (
 )
 from ._esgf import esgf
 from ._history import history
+import warnings
+
+warnings.filterwarnings("always", category=DeprecationWarning, module="freva.*")
+
 
 __all__ = [
     "UserData",
     "databrowser",
+    "count_values",
+    "facet_search",
     "run_plugin",
     "list_plugins",
     "plugin_doc",
--- src/freva/_databrowser.py
+++ src/freva/_databrowser.py
@@ -4,13 +4,259 @@ import json
 from pathlib import Path
 from typing import Any, Optional, Union, Iterator, overload
 from typing_extensions import Literal
+import warnings
 
 import lazy_import
 from evaluation_system.misc import logger
 
 SolrFindFiles = lazy_import.lazy_class("evaluation_system.model.solr.SolrFindFiles")
 
-__all__ = ["databrowser"]
+
+__all__ = ["databrowser", "search_facets", "count_values"]
+
+
+def _proc_search_facets(
+    time_select: Literal["flexible", "strict", "file"] = "flexible",
+    **search_facets: str | list[str] | int,
+) -> dict[str, str | list[str] | int]:
+    """Correct the facet values if needed."""
+    select_methods: dict[str, str] = {
+        "strict": "Within",
+        "flexible": "Intersects",
+        "file": "Contains",
+    }
+    try:
+        search_facets["time_select"] = select_methods[time_select]
+    except KeyError as error:
+        methods = ", ".join(select_methods.keys())
+        raise ValueError(f"Time select method has one of {methods}") from error
+    search_facets["time"] = search_facets.get("time", "")
+    for key in ("file", "uri"):
+        try:
+            search_facets[key] = json.dumps(search_facets[key])
+        except KeyError:
+            pass
+    return search_facets
+
+
+@overload
+def count_values(
+    *,
+    facet: Literal[Union[str, list[str]]],
+    time: str = "",
+    time_select: Literal["strict", "flexible", "file"] = "flexible",
+    multiversion: bool = False,
+    **search_facets: str | list[str] | int,
+) -> dict[str, dict[str, int]]:
+    ...
+
+
+@overload
+def count_values(
+    *,
+    facet: Literal[None],
+    time: str = "",
+    time_select: Literal["strict", "flexible", "file"] = "flexible",
+    multiversion: bool = False,
+    **search_facets: str | list[str] | int,
+) -> int:
+    ...
+
+
+def count_values(
+    *,
+    time: str = "",
+    time_select: Literal["strict", "flexible", "file"] = "flexible",
+    multiversion: bool = False,
+    facet: str | list[str] | None = None,
+    **search_facets: str | list[str] | int,
+) -> int | dict[str, dict[str, int]]:
+    """Count the number of found objects in the databrowser.
+
+    Parameters
+    ----------
+    time: str, default: ""
+        Special search facet to refine/subset search results by time.
+        This can be a string representation of a time range or a single
+        time step. The time steps have to follow ISO-8601. Valid strings are
+        ``%Y-%m-%dT%H:%M`` to ``%Y-%m-%dT%H:%M`` for time ranges and
+        ``%Y-%m-%dT%H:%M``. **Note**: You don't have to give the full string
+        format to subset time steps ``%Y``, ``%Y-%m`` etc are also valid.
+    time_select: str, default: flexible
+        Operator that specifies how the time period is selected. Choose from
+        flexible (default), strict or file. ``strict`` returns only those files
+        that have the *entire* time period covered. The time search ``2000 to
+        2012`` will not select files containing data from 2010 to 2020 with
+        the ``strict`` method. ``flexible`` will select those files as
+        ``flexible`` returns those files that have either start or end period
+        covered. ``file`` will only return files where the entire time
+        period is contained within *one single* file.
+    multiversion: bool, default: False
+        Select all versions and not just the latest version (default).
+    facet: Union[str, list[str]], default: None
+        Count these these facets (attributes & values) instead of the number
+        of total files. If None (default), the number of total files will
+        be returned.
+    **search_facets: str
+        The facets to be applied in the data search. If not given
+        the whole dataset will be queried.
+
+    Returns
+    -------
+    int, dict[str, int]:
+        Number of found objects, if the *facet* key is/are given then the
+        a dictionary with the number of objects for each search facet/key
+        is given.
+
+    Example
+    -------
+    .. execute_code::
+
+        import freva
+        num_files = freva.count_values(experiment="cmorph")
+        print(num_files)
+
+    .. execute_code::
+
+        import freva
+        print(freva.count_values(facet="*"))
+
+    """
+    search_facets = _proc_search_facets(
+        time_select=time_select, time=time, **search_facets
+    )
+    count_all = facet is None
+    if isinstance(facet, str):
+        facet = [facet]
+    if facet in (["*"], ["all"]):
+        facet = []
+    facet = facet or []
+    latest = not multiversion
+    if "version" in search_facets and latest:
+        # it makes no sense to look for a specific version just among the latest
+        # the speedup is marginal and it might not be what the user expects
+        logger.warning("Turning latest off when searching for a specific version.")
+        latest = False
+    core = {True: "latest", False: "files"}[latest]
+    logger.debug("Searching dictionary: %s\n", search_facets)
+    search_facets["facet.limit"] = search_facets.pop("facet_limit", -1)
+    if count_all:
+        with warnings.catch_warnings():
+            warnings.filterwarnings(action="ignore", category=PendingDeprecationWarning)
+            return (
+                SolrFindFiles(core=core)._retrieve_metadata(**search_facets).num_objects
+            )
+    with warnings.catch_warnings():
+        warnings.filterwarnings(action="ignore", category=PendingDeprecationWarning)
+        results = SolrFindFiles(core=core)._facets(facet or None, **search_facets)
+    out: dict[str, dict[str, int]] = {}
+    for att in facet or results.keys():
+        values = results[att]
+        out[att] = {str(v): int(c) for v, c in zip(*[iter(values)] * 2)}
+    return out
+
+
+def facet_search(
+    *,
+    time: str = "",
+    time_select: Literal["strict", "flexible", "file"] = "flexible",
+    multiversion: bool = False,
+    facet: str | list[str] | None = None,
+    **search_facets: str | list[str] | int,
+) -> dict[str, list[str]]:
+    """Search for data attributes (factes) in the databrowser.
+
+    The method queries the databrowser for available search facets (keys)
+    like model, experiment etc.
+
+    Parameters
+    ----------
+    time: str
+        Special search facet to refine/subset search results by time.
+        This can be a string representation of a time range or a single
+        time step. The time steps have to follow ISO-8601. Valid strings are
+        ``%Y-%m-%dT%H:%M`` to ``%Y-%m-%dT%H:%M`` for time ranges and
+        ``%Y-%m-%dT%H:%M``. **Note**: You don't have to give the full string
+        format to subset time steps ``%Y``, ``%Y-%m`` etc are also valid.
+    time_select: str, default: flexible
+        Operator that specifies how the time period is selected. Choose from
+        flexible (default), strict or file. ``strict`` returns only those files
+        that have the *entire* time period covered. The time search ``2000 to
+        2012`` will not select files containing data from 2010 to 2020 with
+        the ``strict`` method. ``flexible`` will select those files as
+        ``flexible`` returns those files that have either start or end period
+        covered. ``file`` will only return files where the entire time
+        period is contained within *one single* file.
+    facet: Union[str, list[str]], default: None
+        Retrieve information about these facets (attributes & values).
+        If None given (default), information about all available facets is
+        returned.
+    multiversion: bool, default: False
+        Select all versions and not just the latest version (default).
+    **search_facets: str
+        The facets to be applied in the data search. If not given
+        the whole dataset will be queried.
+
+    Returns
+    -------
+    dict[str, list[str]]:
+        Dictionary with a list search facet values for each search facet key
+
+
+    Example
+    -------
+
+    .. execute_code::
+
+        import freva
+        all_facets = freva.search_facets(project='obs*')
+        print(all_facets)
+        spec_facets = freva.search_facets(project='obs*',
+                                          facet=["time_frequency", "variable"])
+        print(spec_facets)
+
+    Get all models that have a given time step:
+
+    .. execute_code::
+
+        import freva
+        model = list(freva.search_facets(project="obs*", time="2016-09-02T22:10"))
+        print(model)
+
+    Reverse search: retrieving meta data from a known file
+
+    .. execute_code::
+
+        import freva
+        from pathlib import Path
+        file = ".docker/data/observations/grid/CPC/CPC/cmorph/30min/atmos/30min/r1i1p1/v20210618/pr/pr_30min_CPC_cmorph_r1i1p1_201609020000-201609020030.nc"
+        res = freva.search_facets(file=str(Path(file).absolute()))
+        print(res)
+
+    """
+    search_facets = _proc_search_facets(
+        time_select=time_select, time=time, **search_facets
+    )
+    if isinstance(facet, str):
+        facet = [facet]
+    if facet in (["*"], ["all"]):
+        facet = []
+    facet = facet or []
+    latest = not multiversion
+    if "version" in search_facets and latest:
+        # it makes no sense to look for a specific version just among the latest
+        # the speedup is marginal and it might not be what the user expects
+        logger.warning("Turning latest off when searching for a specific version.")
+        latest = False
+    core = {True: "latest", False: "files"}[latest]
+    logger.debug("Searching dictionary: %s\n", search_facets)
+    search_facets["facet.limit"] = search_facets.pop("facet_limit", -1)
+    with warnings.catch_warnings():
+        warnings.filterwarnings(action="ignore", category=PendingDeprecationWarning)
+        results = SolrFindFiles(core=core)._facets(
+            facets=facet or None, latest_version=False, **search_facets
+        )
+    return {f: v[::2] for f, v in results.items()}
 
 
 @overload
@@ -28,7 +274,7 @@ def databrowser(
     *,
     attributes: Literal[False],
     facet: Union[str, list[str]],
-) -> dict[Any, dict[str, Any]]:
+) -> dict[str, dict[str, Any]]:
     ...
 
 
@@ -53,9 +299,9 @@ def databrowser(
     count: bool = False,
     uniq_key: Literal["file", "uri"] = "file",
     time: str = "",
-    time_select: str = "flexible",
-    **search_facets: Union[str, Path, int, list[str]],
-) -> Union[dict[Any, dict[Any, Any]], Iterator[str], int]:
+    time_select: Literal["flexible", "strict", "file"] = "flexible",
+    **search_facets: Union[str, list[str], int],
+) -> Union[dict[str, dict[str, int]], dict[str, list[str]], Iterator[str], int]:
     """Find data in the system.
 
     You can either search for files or data facets (variable, model, ...)
@@ -157,111 +403,76 @@ def databrowser(
         file_range = freva.databrowser(project="obs*", time="2016-09-02T22:15 to 2016-10", time_select="strict")
         for file in file_range:
             print(file)
-
-
-    Search for facets in the system:
-
-    .. execute_code::
-
-        import freva
-        all_facets = freva.databrowser(project='obs*', all_facets=True)
-        print(all_facets)
-        spec_facets = freva.databrowser(project='obs*',
-                                        facet=["time_frequency", "variable"])
-        print(spec_facets)
-
-    Get all models that have a given time step:
-
-    .. execute_code::
-
-        import freva
-        model = list(freva.databrowser(project="obs*", time="2016-09-02T22:10"))
-        print(model)
-
-    Reverse search: retrieving meta data from a known file
-
-    .. execute_code::
-
-        import freva
-        from pathlib import Path
-        file = ".docker/data/observations/grid/CPC/CPC/cmorph/30min/atmos/30min/r1i1p1/v20210618/pr/pr_30min_CPC_cmorph_r1i1p1_201609020000-201609020030.nc"
-        res = freva.databrowser(file=file, all_facets=True)
-        print(res)
-
-    Check the number of files in the system
-
-    .. execute_code::
-
-        import freva
-        num_files = freva.databrowser(experiment="cmorph", count=True)
-        print(num_files)
-
     """
-    select_methods: dict[str, str] = {
-        "strict": "Within",
-        "flexible": "Intersects",
-        "file": "Contains",
-    }
-    facets: list[str] = []
-    try:
-        search_facets["time_select"] = select_methods[time_select]
-    except KeyError as error:
-        methods = ", ".join(select_methods.keys())
-        raise ValueError(f"Time select method has one of {methods}")
-    search_facets["time"] = time
-    try:
-        # If we don't convert a str to a str mypy will complain.
-        f = Path(str(search_facets["file"])).expanduser().absolute()
-        search_facets["file"] = str(f)
-    except KeyError:
-        pass
-    for key in ("file", "uri"):
-        try:
-            search_facets[key] = json.dumps(search_facets[key])
-        except KeyError:
-            pass
-    if isinstance(facet, str):
-        facet = [facet]
-    facet = facet or []
-    facets += [f for f in facet if f]
-    latest = not multiversion
-    if "version" in search_facets and latest:
-        # it makes no sense to look for a specific version just among the latest
-        # the speedup is marginal and it might not be what the user expects
-        logger.warning("Turning latest off when searching for a specific version.")
-        latest = False
-    core = {True: "latest", False: "files"}[latest]
-    logger.debug("Searching dictionary: %s\n", search_facets)
-    solr_core = SolrFindFiles(core=core)
-    if (facets or all_facets) and not attributes:
-        out = {}
-        search_facets["facet.limit"] = search_facets.pop("facet_limit", -1)
-        for att, values in solr_core._facets(
-            facets=facets or None, latest_version=False, **search_facets
-        ).items():
-            # values come in pairs: (value, count)
-            value_count = len(values) // 2
-            if relevant_only and value_count < 2:
-                continue
-            if count:
-                out[att] = {v: c for v, c in zip(*[iter(values)] * 2)}
-            else:
-                out[att] = values[::2]
-        return out
-    if attributes:
-        # select all is none defined but this flag was set
-        results = solr_core._facets(
-            facets=facets or None, latest_version=False, **search_facets
+    core = {True: "latest", False: "files"}[not multiversion]
+    if (facet or all_facets) and not attributes:
+        warnings.warn(
+            (
+                "The <facet> and <all_facets> arguments will be made "
+                "deprecated from version 2304.0.0 if you want to"
+                "query databrowser facets use the *freva.facet_search* "
+                "method"
+            ),
+            category=DeprecationWarning,
         )
+        if count:
+            return count_values(
+                facet=facet,
+                time=time,
+                time_select=time_select,
+                multiversion=multiversion,
+                **search_facets,
+            )
+        return facet_search(
+            facet=facet,
+            time=time,
+            time_select=time_select,
+            multiversion=multiversion,
+            **search_facets,
+        )
+    if attributes:
+        warnings.warn(
+            (
+                "The <attribute> argument will be made deprecated in version"
+                " 2304.0.0."
+            ),
+            category=DeprecationWarning,
+        )
+        # select all is none defined but this flag was set
+        with warnings.catch_warnings():
+            warnings.filterwarnings(category=PendingDeprecationWarning, action="ignore")
+
+            results = SolrFindFiles(core=core)._facets(
+                facets=facet or None, latest_version=False, **search_facets
+            )
         if relevant_only:
             return (k for k in results if len(results[k]) > 2)
         return (k for k in results)
     if count:
-        return solr_core._retrieve_metadata(**search_facets).num_objects
-    search_results = solr_core._search(
-        batch_size=batch_size,
-        latest_version=latest,
-        uniq_key=uniq_key,
-        **search_facets,
+        warnings.warn(
+            (
+                "The <count> arguments will be made deprecated in "
+                "version 2304.0.0 please use the <freva.count_values> "
+                "method instead."
+            ),
+            category=DeprecationWarning,
+        )
+        return count_values(
+            facet=facet,
+            time=time,
+            time_select=time_select,
+            multiversion=multiversion,
+            **search_facets,
+        )
+    search_facets = _proc_search_facets(
+        time_select=time_select, time=time, **search_facets
     )
+    with warnings.catch_warnings():
+        warnings.filterwarnings(action="ignore", category=PendingDeprecationWarning)
+        search_results = SolrFindFiles(core=core)._search(
+            batch_size=batch_size,
+            latest_version=not multiversion,
+            uniq_key=uniq_key,
+            **search_facets,
+        )
     return search_results
