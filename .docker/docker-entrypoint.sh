1c14dd0c6ddd54ad192845a33fc7c8d6cadc1819
Daniel Black <daniel@mariadb.org>
2024-06-19 11:24:14 +1000

    mariadb: use entrypoint components from mariadb container

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -3,8 +3,25 @@ set -e
 if [ "$VERBOSE" == "yes" ];then
     set -xe
 fi
-mariadbd  &
-/usr/local/bin/docker-entrypoint.sh
+source /usr/local/bin/docker-entrypoint.sh
+# call main bits of the mariadb entrypoint to have DB initialized
+docker_setup_env
+docker_create_db_directories
+# there's no database, so it needs to be initialized
+if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+        docker_verify_minimum_env
+
+        docker_mariadb_init "$@"
+elif _check_if_upgrade_is_needed; then
+        docker_mariadb_upgrade "$@"
+fi
+mariadbd &
+until healthcheck.sh --connect --innodb_initialized;
+do
+    echo "waiting for mariadb"
+    sleep 1
+done
+
 solr start -s ${SOLR_HOME}
 if [ "${IS_BINDER}" = "true" ];then
     python $EVAL_HOME/ingest_dummy_data.py /mnt/data4freva
