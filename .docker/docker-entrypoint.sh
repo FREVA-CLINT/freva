b36d1aad0b0e43460a879332a83bfd93a73c6e3f
Mo <hadizadeh@dkrz.de>
2024-09-17 05:40:10 +0200

    organize the changes for both test and build image

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -3,8 +3,26 @@ set -e
 if [ "$VERBOSE" == "yes" ];then
     set -xe
 fi
-# start mariadb in the created image
-bash /tmp/mysql_init &
+source /usr/local/bin/docker-entrypoint.sh
+set -- mariadbd
+# call main bits of the mariadb entrypoint to have DB initialized
+docker_setup_env "$@"
+docker_create_db_directories "$@"
+# there's no database, so it needs to be initialized
+if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
+    docker_verify_minimum_env "$@"
+    docker_mariadb_init "$@"
+elif _check_if_upgrade_is_needed; then
+    docker_mariadb_upgrade "$@"
+fi
+mariadbd --user=${MARIADB_USER} --datadir=${MYSQL_DATA_DIR} --socket=/tmp/mysql.sock --console &
+until mariadb-admin ping --user="${MARIADB_USER}" --password="${MARIADB_PASSWORD}" --socket="/tmp/mysql.sock" --silent;
+do
+    echo "waiting for mariadb"
+    echo "${MARIADB_USER} ${MARIADB_PASSWORD}"
+    sleep 1
+done
+
 solr start -s ${SOLR_HOME}
 if [ "${IS_BINDER}" = "true" ];then
     # wait for solr to be up
@@ -17,7 +35,7 @@ if [ "${IS_BINDER}" = "true" ];then
     python $EVAL_HOME/ingest_dummy_data.py /mnt/data4frev
     python $EVAL_HOME/dummy_user_data.py
     for i in 1 2 3 4 5; do
-        freva plugin dummyplugin the_number=$i -vvv
+        freva plugin dummyplugin the_number=$i
     done
 fi
 exec "$@"
